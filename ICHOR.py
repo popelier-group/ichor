0190e340b179bb2de59e6064936cbae2a3f3f787
Matt Burn <matthew.burn@postgrad.manchester.ac.uk>
2019-11-20 15:17:39 +0000

    Update ICHOR.py

--- ICHOR.py
+++ ICHOR.py
@@ -222,7 +222,7 @@ dlpoly_weights =  {"H": 1.007975, "He": 4.002602, "Li": 6.9675, "Be": 9.0121831,
 logging.basicConfig(filename='ichor.log',
                     level=logging.DEBUG, 
                     format='%(asctime)s - %(levelname)s - %(message)s', 
-                    datefmt='%d-%m-%Y %I:%M:%S')
+                    datefmt='%d-%m-%Y %H:%M:%S')
 
 _unknown_settings = []
 
