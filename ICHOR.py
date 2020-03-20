35a6743809768207e6608f7406402ec4c18aaf82
MattBurn <43954274+MattBurn@users.noreply.github.com>
2020-03-20 10:37:45 +0000

    Update ICHOR.py

--- ICHOR.py
+++ ICHOR.py
@@ -3158,7 +3158,7 @@ class Point:
     def multipole_training_set_line(self):
         training_set = self.__get_features()
         for atom in training_set.keys():
-            training_set[atom]["outputs"] = list(self.ints[atom].multipoles.values())
+            training_set[atom]["outputs"] = list(self.ints[atom].multipoles.values())[:25]
         return training_set
 
     def move(self, new_directory):
