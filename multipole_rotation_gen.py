7fd57c6d51a573effa31c75d42cc642075d35c1f
MattBurn <43954274+MattBurn@users.noreply.github.com>
2020-09-21 12:27:58 +0100

    Added multipole rotation to int files

---
+++ multipole_rotation_gen.py
@@ -0,0 +1,71 @@
+d = {"x": 0, "y": 1, "z": 2}
+axes = ["x", "y", "z"]
+
+def indices_to_index(*indices):
+    o = ""
+    for i in indices:
+        o += axes[i]
+    return o
+
+def indices_to_index_dict(d, *indices):
+    o = ""
+    for i in indices:
+        o += axes[i]
+    return d[o]
+
+with open("multipole.txt", "w") as f:
+    r = []
+    order = ["xx", "xy", "xz", "yy", "yz", "zz"]
+    for i in range(3):
+        for j in range(3):
+            i_j = list(sorted([i, j]))
+            if not i_j in r:
+                print(i_j, r)
+                r.append(i_j)
+                idx = indices_to_index(i, j)
+                q = f"Q_{idx} = "
+                for a in range(3):
+                    for b in range(3):
+                        idx = indices_to_index(*sorted([a, b]))
+                        q += f" C[{i}][{a}]*C[{j}][{b}]*q_{idx} +"
+                f.write(q.rstrip("+") + "\n")
+    f.write("\n")
+    r = []
+    order = ["xxx", "xxy", "xxz", "xyy", "xyz", "xzz", "yyy", "yyz", "yzz", "zzz"]
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                i_j_k = list(sorted([i, j, k]))
+                if not i_j_k in r:
+                    print(i_j_k, r)
+                    r.append(i_j_k)
+                    idx = indices_to_index(i, j, k)
+                    o = f"O_{idx} = "
+                    for a in range(3):
+                        for b in range(3):
+                            for c in range(3):
+                                idx = indices_to_index(*sorted([a, b, c]))
+                                o += f" C[{i}][{a}]*C[{j}][{b}]*C[{k}][{c}]*o_{idx} +"
+                    f.write(o.rstrip("+") + "\n")
+    f.write("\n")
+    r = []
+    order = ["xxxx", "xxxy", "xxxz", "xxyy", "xxyz", "xxzz", "xyyy", "xyyz", "xyzz", "xzzz", "yyyy", "yyyz", "yyzz", "yzzz", "zzzz"]
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    i_j_k_l = list(sorted([i, j, k, l]))
+                    if not i_j_k_l in r:
+                        print(i_j_k_l, r)
+                        r.append(i_j_k_l)
+                        idx = indices_to_index(i, j, k, l)
+                        h = f"H_{idx} = "
+                        for a in range(3):
+                            for b in range(3):
+                                for c in range(3):
+                                    for d in range(3):
+                                        idx = indices_to_index(*sorted([a, b, c, d]))
+                                        h += f" C[{i}][{a}]*C[{j}][{b}]*C[{k}][{c}]*C[{l}][{d}]*h_{idx} +"
+                        f.write(h.rstrip("+") + "\n")
+
+
