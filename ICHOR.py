706ccf5546ddc3d169d5e30d5da1fc953489bddd
MattBurn <43954274+MattBurn@users.noreply.github.com>
2021-03-04 11:19:59 +0000

    Added DlpolyHistory File

--- ICHOR.py
+++ ICHOR.py
@@ -10629,6 +10629,32 @@ class Trajectory(Points):
         return self._trajectory[i]
 
 
+class DlpolyHistory(Trajectory):
+    @buildermethod
+    def read(self, n=-1, write_to_traj=True):
+        with open(self.path, "r") as f:
+            for line in f:
+                if line.startswith("timestep"):
+                    natoms = int(line.split()[2])
+                    line = next(f) # x unit vector
+                    line = next(f) # y unit vector
+                    line = next(f) # z unit vector
+
+                    atoms = Atoms()
+                    while len(atoms) < natoms:
+                        line = next(f) # Atom Line
+                        atom_type = line.split()[0]
+                        line = next(f) # Coordinate Line
+                        atoms.add(f"{atom_type} {line}")
+        if write_to_traj:
+            self.write()
+
+    def write(self, fname=None):
+        if fname is None:
+            fname = self.path.parent / Path("TRAJECTORY.xyz")
+        super().write(fname=fname)
+
+
 class PointTools:
     @staticmethod
     def select_wfn():
