26d5f985e6d1a353f7671372152db8ad5a403e0b
m-julian <52214154+m-julian@users.noreply.github.com>
2021-08-10 12:03:17 +0300

    added dlpoly history comments and todos

--- ichor/files/dlpoly_history.py
+++ ichor/files/dlpoly_history.py
@@ -3,6 +3,11 @@ from ichor.files.trajectory import Trajectory
 
 
 class DlpolyHistory(Trajectory):
+    """ Wraps around a DL POLY History file, which just contains timesteps 
+    with different molecular configurations. Essentially the same contents as a
+    .xyz trajectory file, but in a different format."""
+
+    # overload the Trajectory _read_file method because the formatting of the history file is different
     def _read_file(self):
         with open(self.path, "r") as f:
             for line in f:
@@ -22,9 +27,13 @@ class DlpolyHistory(Trajectory):
                     self.add(atoms)
 
     def write(self, fname=None):
+        """ Writes a trajectory .xyz file from the DL POLY History file."""
+        # matt_todo: Path() is not needed here
         if fname is None:
             fname = self.path.parent / Path("TRAJECTORY.xyz")
         super().write(fname=fname)
 
-    def write_to_trajectory(self):
+    def write_to_trajectory(self) -> None:
+        """ See the `DlpolyHistory` `write` method which writes a .xyz trajectory
+        from a DL POLY History File."""
         self.write()
