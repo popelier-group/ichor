173f69ad42c72cdf7d4f8f2b487d45c0a820f525
Matthew Burn <43954274+MattBurn@users.noreply.github.com>
2021-09-07 14:29:53 +0100

    Changed ActiveLearningMethod to a lazy evealuation

--- ichor/active_learning/__init__.py
+++ ichor/active_learning/__init__.py
@@ -1,6 +1,6 @@
 from ichor.active_learning.expected_improvement import ExpectedImprovement
 from ichor.active_learning.mepe import MEPE
-from ichor.globals import GLOBALS
+from ichor.common.functools import lazy
 
 active_learning_methods = {
     active_learning_method.name: active_learning_method
@@ -8,6 +8,14 @@ active_learning_methods = {
     if isinstance(active_learning_method, ExpectedImprovement)
 }
 
-ActiveLearningMethod = active_learning_methods[GLOBALS.ACTIVE_LEARNING_METHOD]
+
+# implemented as a lazy function to prevent circular dependency
+@lazy
+def _get_active_learning_method() -> ExpectedImprovement:
+    from ichor.globals import GLOBALS
+    return active_learning_methods[GLOBALS.ACTIVE_LEARNING_METHOD]
+
+
+ActiveLearningMethod = _get_active_learning_method()
 
 __all__ = ["ActiveLearningMethod"]
