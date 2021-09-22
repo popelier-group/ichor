448767d68d7ad56ad76fac9e6af5f1500be3a1d9
Matthew Burn <43954274+MattBurn@users.noreply.github.com>
2021-09-22 11:05:11 +0100

    Added dlpoly menu to analysis menu

--- ichor/analysis/analysis_menu.py
+++ ichor/analysis/analysis_menu.py
@@ -1,5 +1,6 @@
 from ichor.analysis.rmse import rmse_menu
 from ichor.analysis.s_curves import s_curve_menu
+from ichor.analysis.dlpoly import dlpoly_menu
 from ichor.menu import Menu
 
 
@@ -7,3 +8,4 @@ def analysis_menu():
     with Menu("Analysis Menu", space=True, back=True, exit=True) as menu:
         menu.add_option("s", "s-curve analysis", s_curve_menu)
         menu.add_option("rmse", "rmse analysis", rmse_menu)
+        menu.add_option("dlpoly", "dlpoly analysis", dlpoly_menu)
--- ichor/analysis/dlpoly/__init__.py
+++ ichor/analysis/dlpoly/__init__.py
@@ -1,7 +1,9 @@
+from ichor.analysis.dlpoly.dlpoly_menu import dlpoly_menu
 from ichor.analysis.dlpoly.dlpoly_analysis import (
     get_dlpoly_energies, run_dlpoly_geometry_optimisations, submit_final_geometry_to_gaussian)
 
 __all__ = [
+    "dlpoly_menu",
     "run_dlpoly_geometry_optimisations",
     "get_dlpoly_energies",
     "submit_final_geometry_to_gaussian",
