cdfb621cac811e0ba6f2e1e47ffd0da6cf609702
m-julian <52214154+m-julian@users.noreply.github.com>
2021-09-30 21:37:12 +0300

    added docstrings

--- ichor/files/xyz.py
+++ ichor/files/xyz.py
@@ -7,6 +7,14 @@ from ichor.files.geometry import GeometryFile
 
 
 class XYZ(GeometryFile, File):
+    """ A class which wraps around a .xyz file that is contained in each PointDirectory. This .xyz file should always be there and it is
+    used to write out .gjf files. Each instance is 
+    
+    :param path: The path to an .xyz file
+    :param atoms: Optional list of Atoms which can be used to construct a .xyz file. If a list of atoms is passed, then a new xyz file
+        with the given Atoms will be written to the given Path.
+    """
+
     def __init__(self, path: Path, atoms: Optional[Atoms] = None):
         File.__init__(self, path)
 
@@ -19,6 +27,7 @@ class XYZ(GeometryFile, File):
         return '.xyz'
 
     def _read_file(self):
+        """ Read a .xyz file and constructs the `self.atoms` attribute which is an instance of `Atoms`"""
         with open(self.path, 'r') as f:
             natoms = int(next(f))
             _ = next(f)  # blank line
@@ -28,6 +37,10 @@ class XYZ(GeometryFile, File):
                 self.atoms.add(Atom(record[0], float(record[1]), float(record[2]), float(record[3])))
 
     def write(self, path: Optional[Path] = None):
+        """ Write a .xyz to a given path. If no path is given, it will write it to the path given when the XYZ instance was constructed.
+        
+        :param path: An optional path to which to write the .xyz file
+        """
         if path is None:
             path = self.path
         with open(path, 'w') as f:
