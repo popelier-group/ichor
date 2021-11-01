eb8d25b93bb39a101ce7de4bf8e07571b94fe49c
m-julian <52214154+m-julian@users.noreply.github.com>
2021-11-01 16:15:27 +0200

    added sklearn example code

--- test/test_rbf_kernel.py
+++ test/test_rbf_kernel.py
@@ -28,6 +28,16 @@ class TestRBF(unittest.TestCase):
         cov_matrix = kernel.k(x1, x2)
 
         # sklearn 0.24.2
+        # import numpy as np
+        # from sklearn.gaussian_process.kernels import RBF
+
+        # lengthscale = np.array([2.0, 2.0, 2.0, 2.0])
+        # x1 = np.array([[1.2, 1.6, 2.24, 4.21], [0.9, 1.2, 2.03, 3.56], [1.31, 1.25, 2.01, 5.34]])
+        # x2 = np.array([[1.3, 1.35, 2.20, 5.12], [0.81, 1.09, 1.98, 5.49], [1.61, 1.87, 3.56, 6.01]])
+
+        # kernel = RBF(length_scale=lengthscale)
+        # cov_matrix = kernel(x1, x2)
+
         sklearn_rbf_cov_matrix_one_lengthscale = np.array([[0.89335164, 0.76737859, 0.52052102],
                                                             [0.71847455, 0.62597177, 0.31284419],
                                                             [0.98824464, 0.96331482, 0.65986171]])
@@ -45,6 +55,17 @@ class TestRBF(unittest.TestCase):
         cov_matrix = kernel.k(x1, x2)
 
         # sklearn 0.24.2
+        # import numpy as np
+        # from sklearn.gaussian_process.kernels import RBF
+
+        # lengthscale = np.array([4.3, 2.14, 3.06, 1.73])
+        # x1 = np.array([[1.2, 1.6, 2.24, 4.21], [0.9, 1.2, 2.03, 3.56], [1.31, 1.25, 2.01, 5.34]])
+        # x2 = np.array([[1.3, 1.35, 2.20, 5.12], [0.81, 1.09, 1.98, 5.49], [1.61, 1.87, 3.56, 6.01]])
+
+        # kernel = RBF(length_scale=lengthscale)
+        # cov_matrix = kernel(x1, x2)
+        # print(cov_matrix)
+
         sklearn_rbf_cov_matrix_multiple_lengthscales = np.array([[0.86456942, 0.73356806, 0.52370482],
                                                             [0.66041384, 0.53581677, 0.30408973],
                                                             [0.98895348, 0.98672651, 0.78060504]])
@@ -63,6 +84,18 @@ class TestRBF(unittest.TestCase):
         cov_matrix = kernel.k(x1, x2)
 
         # sklearn 0.24.2
+        # import numpy as np
+        # from sklearn.gaussian_process.kernels import RBF
+
+        # active_dims = np.array([0,1,2])
+
+        # lengthscale = np.array([4.3, 2.14, 3.06, 1.73])
+        # x1 = np.array([[1.2, 1.6, 2.24, 4.21], [0.9, 1.2, 2.03, 3.56], [1.31, 1.25, 2.01, 5.34]])
+        # x2 = np.array([[1.3, 1.35, 2.20, 5.12], [0.81, 1.09, 1.98, 5.49], [1.61, 1.87, 3.56, 6.01]])
+
+        # kernel = RBF(length_scale=lengthscale[active_dims])
+        # cov_matrix = kernel(x1[:, active_dims], x2[:,active_dims])
+
         sklearn_rbf_cov_matrix_with_active_dims = np.array([[0.99284612, 0.96452413, 0.89983304],
                                                             [0.99170815, 0.99832779, 0.82891097],
                                                             [0.99698238, 0.99044249, 0.84139688]])
