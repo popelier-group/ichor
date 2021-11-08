01703a7d6af8c43a959dcae73e89c23a97dc4b55
Matthew Burn <43954274+MattBurn@users.noreply.github.com>
2021-11-08 11:48:12 +0000

    Convert config file to path

--- ichor/globals/__init__.py
+++ ichor/globals/__init__.py
@@ -2,6 +2,8 @@
 defined in globals.py, but these can be overwritten using an ICHOR config file. GLOBALS also determine imporatnt values/keywords depending on the machine (CSF3/FFLUXLAB)
 that ICHOR is being ran on."""
 
+from pathlib import Path
+
 from ichor.common.os import input_with_prefill
 from ichor.globals.globals import Globals
 from ichor.globals.os import OS
@@ -97,8 +99,7 @@ def edit_config_location():
         config_location = input(">> ")
     else:
         config_location = input_with_prefill(">> ", GLOBALS._config_file)
-    GLOBALS._config_file = config_location
-
+    GLOBALS._config_file = Path(config_location)
 
 
 def settings_menu_refresh(menu):
