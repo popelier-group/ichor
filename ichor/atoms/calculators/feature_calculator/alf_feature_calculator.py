9d0fc6608225bf81f820537cf6c1820f30ab3af3
m-julian <52214154+m-julian@users.noreply.github.com>
2021-11-10 16:42:03 +0200

    feature calculations work for 2 atom systems

--- ichor/atoms/calculators/feature_calculator/alf_feature_calculator.py
+++ ichor/atoms/calculators/feature_calculator/alf_feature_calculator.py
@@ -244,7 +244,13 @@ class ALFFeatureCalculator(FeatureCalculator):
             :type: `np.ndarray`
                 A 1D numpy array of shape 3N-6, where N is the number of atoms in the system which `atom` is a part of.
         """
-        feature_array = np.empty(3 * len(atom.parent) - 6)
+        
+        if len(atom.parent) == 2:
+            feature_array = np.empty(1)  # if only 2 atoms are in parent, there are only 2 atoms in the system so there is only 1 feature - distance.
+        elif len(atom.parent) > 2:
+            feature_array = np.empty(3 * len(atom.parent) - 6)  # most systems have more than 2 atoms
+        else:
+            raise ValueError("atom.parent needs to have more than 1 atom in order to calculate features.")
 
         # Feature calculation assumes units are in angstroms
         atom.to_angstroms()
@@ -254,16 +260,23 @@ class ALFFeatureCalculator(FeatureCalculator):
         )
 
         x_axis_atom = cls.calculate_x_axis_atom(atom, alf)
-        xy_plane_atom = cls.calculate_xy_plane_atom(atom, alf)
-
         x_axis_vect = unit_conversion * (
             x_axis_atom.coordinates - atom.coordinates
         )
+        x_bond_norm = np.linalg.norm(x_axis_vect)
+        
+        if len(atom.parent) == 2:
+            
+            feature_array[0] = x_bond_norm
+            return feature_array
+        
+        # this code is only needed if atom.parent is more than 2 atoms (so it has 3N-6 features)
+        xy_plane_atom = cls.calculate_xy_plane_atom(atom, alf)
+        
         xy_plane_vect = unit_conversion * (
             xy_plane_atom.coordinates - atom.coordinates
         )
-
-        x_bond_norm = np.linalg.norm(x_axis_vect)
+        
         xy_bond_norm = np.linalg.norm(xy_plane_vect)
 
         angle = np.arccos(
@@ -305,4 +318,5 @@ class ALFFeatureCalculator(FeatureCalculator):
                 feature_array[i_feat] = np.arctan2(zeta[1], zeta[0])
 
                 i_feat += 1
+                
         return feature_array
