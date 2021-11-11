2f09337cb31c14846babf1fb967861cd05f06a08
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2021-11-11 16:24:08 +0000

    Update list_of_atoms.py

--- ichor/atoms/list_of_atoms.py
+++ ichor/atoms/list_of_atoms.py
@@ -115,13 +115,8 @@ class ListOfAtoms(list):
             If the trajectory instance is indexed by str, the array has shape `n_atoms` x `n_features`.
             If the trajectory instance is indexed by slice, the array has shape `slice`, `n_atoms` x `n_features`.
         """
-        
-        from ichor.files import PointsDirectory, Trajectory
-        if isinstance(self, Trajectory):
-            features = np.array([timestep.alf_features(alf) for timestep in self])
-        elif isinstance(self, PointsDirectory):
-            features = np.array([point.alf_features(alf) for point in self])
-            
+
+        features = np.array([point.alf_features(alf) for point in self])
         if features.ndim == 3:
             features = np.transpose(features, (1, 0, 2))
         return features
