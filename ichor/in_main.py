0638280c92f14b4c5c0af5596ceca023a9968d37
m-julian <52214154+m-julian@users.noreply.github.com>
2021-11-16 18:03:02 +0200

    docstring update

--- ichor/in_main.py
+++ ichor/in_main.py
@@ -1,5 +1,4 @@
-""" This is used to check if ichor is launched as command line interface (CLI) or used as a library. The qcp.py directly below the ichor directory
-sets the value of _IN_MAIN. If ichor3.py is the file that is being ran, then ICHOR is ran as CLI, so the class Arguments needs to be used. If ichor is used
+""" This is used to check if ichor is launched as command line interface (CLI) or used as a library. If ichor3.py is the file that is being ran, then ICHOR is ran as CLI, so the class Arguments needs to be used. If ichor is used
 as a library, then the Arguments class is not used. This is needed to prevent cyclic imports or settings overwriting that Arguments does."""
 
 from functools import wraps
