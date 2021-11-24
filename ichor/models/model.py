Fixed likelihood calculation

ichor/models/model.py
@@ -318,7 +318,7 @@ class Model(File):
 
     @cached_property
     def _y_minus_mean(self):
-        return self.y - self.mean.value(self.x)
+        return self.y - self.mean.value(self.x).reshape((-1, 1))
 
     @cached_property
     def logdet(self):
@@ -331,7 +331,7 @@ class Model(File):
 
     def compute_likelihood(self) -> float:
         """Computes the marginal likelihood from the data given"""
-        return 0.5*np.dot(self._y_minus_mean.T, self.compute_weights()) - 0.5*self.logdet - 0.5*self.ntrain*np.log(2*np.pi)
+        return 0.5*np.dot(self._y_minus_mean.T, self.compute_weights()).item() - 0.5*self.logdet - 0.5*self.ntrain*np.log(2*np.pi)
 
     def predict(self, x_test: np.ndarray) -> np.ndarray:
         """Returns an array containing the test point predictions."""
@@ -359,73 +359,74 @@ class Model(File):
         if path.is_dir():
             path = path / f"{self.system}_{self.type}_{self.atom}{Model.filetype}"
 
-        self.program = self.program if self.program is not FileContents else "ichor"
-        self.program_version = self.program_version if self.program_version is not FileContents else __version__
-        self.nugget = self.nugget if self.nugget is not FileContents else 1e-10
+        with self.block():
+            self.program = self.program if self.program is not FileContents else "ichor"
+            self.program_version = self.program_version if self.program_version is not FileContents else __version__
+            self.nugget = self.nugget if self.nugget is not FileContents else 1e-10
 
-        self.system = self.system if self.system is not FileContents else GLOBALS.SYSTEM_NAME
-        self.atom = self.atom if self.atom is not FileContents else "X1"
-        self.type = self.type if self.type is not FileContents else "p1"
-        self.alf = self.alf if self.alf is not FileContents else [1, 1, 1]
+            self.system = self.system if self.system is not FileContents else GLOBALS.SYSTEM_NAME
+            self.atom = self.atom if self.atom is not FileContents else "X1"
+            self.type = self.type if self.type is not FileContents else "p1"
+            self.alf = self.alf if self.alf is not FileContents else [1, 1, 1]
 
-        if self.x is not FileContents:
-            if self.x.ndim == 1:
-                self.x = self.x.reshape(1, -1)
-            elif self.x.ndim != 2:
-                raise ValueError(f"Training Input (x) must be 2D, {self.x.ndim}D array encountered")
+            if self.x is not FileContents:
+                if self.x.ndim == 1:
+                    self.x = self.x.reshape(1, -1)
+                elif self.x.ndim != 2:
+                    raise ValueError(f"Training Input (x) must be 2D, {self.x.ndim}D array encountered")
+                if self.nfeats is FileContents:
+                    self.nfeats = self.x.shape[1]
+                if self.ntrain is FileContents:
+                    self.ntrain = self.x.shape[0]
+
+            if self.nfeats is FileContents and self.natoms is not FileContents:
+                self.nfeats = 3*self.natoms - 6
+
+            if self.natoms is FileContents and self.nfeats is not FileContents:
+                self.natoms = (self.nfeats + 6) // 3
+
+            if self.natoms is FileContents:
+                self.natoms = 1
+
             if self.nfeats is FileContents:
-                self.nfeats = self.x.shape[1]
+                self.nfeats = 0
+
             if self.ntrain is FileContents:
-                self.ntrain = self.x.shape[0]
+                self.ntrain = 0
 
-        if self.nfeats is FileContents and self.natoms is not FileContents:
-            self.nfeats = 3*self.natoms - 6
+            if self.x is FileContents:
+                self.x = np.zeros((self.ntrain, self.nfeats))
 
-        if self.natoms is FileContents and self.nfeats is not FileContents:
-            self.natoms = (self.nfeats + 6) // 3
+            if self.y is FileContents:
+                self.y = np.zeros((self.ntrain, 1))
 
-        if self.natoms is FileContents:
-            self.natoms = 1
+            if self.input_units is FileContents:
+                self.input_units = _get_default_input_units(self.nfeats)
 
-        if self.nfeats is FileContents:
-            self.nfeats = 0
+            if self.output_unit is FileContents:
+                self.output_unit = _get_default_output_unit(self.type)
 
-        if self.ntrain is FileContents:
-            self.ntrain = 0
+            if self.kernel is FileContents:
+                self.kernel = RBFCyclic("k1", np.ones(self.nfeats))
 
-        if self.x is FileContents:
-            self.x = np.zeros((self.ntrain, self.nfeats))
+            if self.mean is FileContents:
+                if self.ntrain == 0:
+                    self.mean = ConstantMean(0.0)
+                else:
+                    self.mean = ConstantMean(np.mean(self.y.flatten()))
 
-        if self.y is FileContents:
-            self.y = np.zeros((self.ntrain, 1))
+            if self.likelihood is FileContents:
+                self.likelihood = self.compute_likelihood()
 
-        if self.input_units is FileContents:
-            self.input_units = _get_default_input_units(self.nfeats)
-
-        if self.output_unit is FileContents:
-            self.output_unit = _get_default_output_unit(self.type)
-
-        if self.kernel is FileContents:
-            self.kernel = RBFCyclic("k1", np.ones(self.nfeats))
-
-        if self.mean is FileContents:
-            if self.ntrain == 0:
-                self.mean = ConstantMean(0.0)
-            else:
-                self.mean = ConstantMean(np.mean(self.y.flatten()))
-
-        if self.likelihood is FileContents:
-            self.likelihood = self.compute_likelihood()
-
-        if self.weights is FileContents:
-            self.weights = self.compute_weights()
+            if self.weights is FileContents:
+                self.weights = self.compute_weights()
 
         with open(path, 'w') as f:
             f.write("# [metadata]\n")
             f.write(f"# program {self.program}\n")
             f.write(f"# version {self.program_version}\n")
             f.write(f"# nugget {self.nugget}\n")
-            f.write(f"# nugget {self.likelihood}\n")
+            f.write(f"# likelihood {self.likelihood}\n")
             for key, val in self.notes.items():
                 f.write(f"# {key} = {val}\n")
             f.write("\n")