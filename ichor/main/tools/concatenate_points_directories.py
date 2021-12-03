2c0c035c28a72773bde9ce6e4b1ec0ebf209ab11
Matthew Burn <43954274+MattBurn@users.noreply.github.com>
2021-12-03 13:30:55 +0000

    Added recurssive rename to concatenate points directory

--- ichor/main/tools/concatenate_points_directories.py
+++ ichor/main/tools/concatenate_points_directories.py
@@ -3,15 +3,23 @@ from pathlib import Path
 from ichor.globals import GLOBALS
 
 
+def recurssive_rename(dir: Path, orig: str, updated: str):
+    for f in dir.iterdir():
+        if orig in f.name:
+            newf = Path(str(f).replace(f.name, f.name.replace(orig, updated)))
+            f.rename(newf)
+        if f.is_dir():
+            recurssive_rename(f, orig, updated)
+
+
 def concatenate_points_directories(pd1: Path, pd2: Path) -> PointsDirectory:
     pd2 = PointsDirectory(pd2)
     for point in pd2:
         points = PointsDirectory(pd1)
-        new_directory = (
-                pd1
-                / f"{GLOBALS.SYSTEM_NAME}{str(len(points) + 1).zfill(4)}"
-        )
+        new_name = f"{GLOBALS.SYSTEM_NAME}{str(len(points) + 1).zfill(4)}"
+        new_directory =  pd1 / new_name
         point.move(new_directory)
+        recurssive_rename(new_directory, point.path.name, new_name)
     return PointsDirectory(pd1)
 
 
