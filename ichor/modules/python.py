c22f5b9917ec58e4a1386bc6d13f13b7960f88bb
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2022-01-13 09:49:05 +0000

    Added ffluxlab python module

--- ichor/modules/python.py
+++ ichor/modules/python.py
@@ -4,3 +4,5 @@ from ichor.modules.modules import Modules
 PythonModules = Modules()
 
 PythonModules[Machine.csf3] = ["apps/anaconda3/5.2.0/bin"]
+
+PythonModules[Machine.ffluxlab] = ["apps/anaconda3/3.7.6"]
