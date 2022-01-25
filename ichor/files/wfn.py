f9a94ab11882432b0de74e9db6181ae08052a886
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2022-01-25 17:55:55 +0000

    Update wfn.py

--- ichor/files/wfn.py
+++ ichor/files/wfn.py
@@ -54,9 +54,9 @@ class WFN(GeometryFile, GeometryDataFile, File):
                         # WFN line using free-formatting
                         record = line.split()
                         atom_type = record[0]
-                        x = float(record[3])
-                        y = float(record[4])
-                        z = float(record[5])
+                        x = float(record[4])
+                        y = float(record[5])
+                        z = float(record[6])
 
                     self.atoms.add(
                         Atom(atom_type, x, y, z, units=AtomicDistance.Bohr)
