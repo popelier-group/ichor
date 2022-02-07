9c873d8023fb67c5492891f8a03398e04080926f
m-julian <52214154+m-julian@users.noreply.github.com>
2022-02-07 19:01:46 +0200

    remove globals, works for aimall v17
    * Removed the need for GLOBALS in the class to make in more straightforward to use class in external scripts
    * Reduced code because v17 and v19 AIMALL .aim files have the same data in them

--- ichor/files/aim.py
+++ ichor/files/aim.py
@@ -1,8 +1,5 @@
-from http.client import LineTooLong
 from pathlib import Path
-from pickle import GLOBAL
 from typing import Optional, Union
-
 from ichor.common.functools import classproperty
 from ichor.common.str import get_digits
 from ichor.common.types import Version
@@ -56,112 +53,72 @@ class AIM(File, dict):
     def _read_file(self):
 
         self.license_check_succeeded = False
-        version_read = False
 
         with open(self.path, "r") as f:
 
-            for line in f:
+            is_all_atom_calculation = False
+
+            lines = f.readlines()
+            for line in lines:
+                if "aimint.exe" in line:
+                    is_all_atom_calculation = True
+
+            for line in lines:
 
                 if "AIMAll Professional license check succeeded." in line:
                     self.license_check_succeeded = True
-                    continue
                 elif "AIMQB (Version" in line:
                     self.version = Version(line.split()[2])
-                    version_read = True
-                    continue
+                elif "Wavefunction File:" in line:
+                    self.wfn = Path(line.split()[-1])
+                elif "Number of processors used for this job =" in line:
+                    self.nproc = int(line.split()[-1])
+                elif "Number of NACPs  =" in line:
+                    self.nacps = int(line.split()[-1])
+                elif "Number of NNACPs =" in line:
+                    self.nnacps = int(line.split()[-1])
+                elif "Number of BCPs  =" in line:
+                    self.nbcps = int(line.split()[-1])
+                elif "Number of RCPs  =" in line:
+                    self.nrcps = int(line.split()[-1])
+                elif "Number of CCPs  =" in line:
+                    self.nccps = int(line.split()[-1])
 
-                # have to have this check, otherwise file will not be read correctly
-                # this is because once reading has started, FileState is Changed to FileState.Reading
-                # then self.version cannot be found because the file has not been fully read yet
-                if version_read:
+                if is_all_atom_calculation:
 
-                    # TODO: check these for version 17, because I (YM) am using v 19.
-                    if self.version.major == 17:
+                    # Check which .int files should be produced.
+                    # if AIMALL does the partitioning for more than 1 atom, then it does not write out a line with `Out File`
+                    if "aimint.exe" in line:
+                        # find the path to where the atom information will be stored (this is without suffix), also remove extra " in line
+                        aimatom_path = Path(line.split()[-2].strip('"'))
+                        # convert the stem to upper, eg. o1 -> O1
+                        inpfile = aimatom_path.with_suffix(".inp")
+                        outfile = aimatom_path.with_suffix(".int")
+                        atom_name = aimatom_path.stem.upper()
+                        self[atom_name] = AimAtom(
+                            atom_name=atom_name, inp_file=inpfile, int_file=outfile
+                        )
+                    elif "Total time for atom" in line:
+                        record = line.split()
+                        atom_name = record[4]
+                        self[atom_name].time_taken = int(record[6])
+                        self[atom_name].integration_error = float(record[-1])
 
-                        if "Wavefunction File:" in line:
-                            self.wfn = Path(line.split()[-1])
-                        elif "Number of processors used for this job =" in line:
-                            self.nproc = int(line.split()[-1])
-                        elif "Number of NACPs  =" in line:
-                            self.nacps = int(line.split()[-1])
-                        elif "Number of NNACPs =" in line:
-                            self.nbacps = int(line.split()[-1])
-                        elif "Number of NBCPs  =" in line:
-                            self.nbcps = int(line.split()[-1])
-                        elif "Number of NRCPs  =" in line:
-                            self.nrcps = int(line.split()[-1])
-                        elif "Number of NCCPs  =" in line:
-                            self.nccps = int(line.split()[-1])
-                        elif "Inp File:" in line:  # AIMAll 19
-                            inpfile = Path(line.split()[-1])
-                            self[inpfile.stem] = AimAtom(
-                                atom=inpfile.stem, inpfile=inpfile
-                            )
-                        elif "Out File:" in line:  # AIMAll 19
-                            outfile = Path(line.split()[-1])
-                            self[outfile.stem].outfile = outfile
-                        elif "Time Taken" in line:
-                            record = line.split()
-                            atom, time_taken, integration_error = (
-                                record[4],
-                                float(record[6]),
-                                float(record[8]),
-                            )  # todo: check these
-                            self[atom].time_taken = time_taken
-                            self[atom].integration_error = integration_error
+                # if AIMALL only performs partitioning for one atom there are different line in the .aim file
+                else:
+                    if "Inp File:" in line:
+                        inpfile = Path(line.split()[-1].strip('"'))
+                        outfile = inpfile.with_suffix(".int")
+                        atom_name = inpfile.stem.upper()
+                        self[atom_name] = AimAtom(
+                            atom_name=atom_name, inp_file=inpfile, int_file=outfile
+                        )
 
-                    elif self.version.major == 19:
-
-                        if "Wavefunction File:" in line:
-                            self.wfn = Path(line.split()[-1])
-                        elif "Number of processors used for this job =" in line:
-                            self.nproc = int(line.split()[-1])
-                        elif "Number of NACPs  =" in line:
-                            self.nacps = int(line.split()[-1])
-                        elif "Number of NNACPs =" in line:
-                            self.nbacps = int(line.split()[-1])
-                        elif "Number of BCPs  =" in line:
-                            self.nbcps = int(line.split()[-1])
-                        elif "Number of RCPs  =" in line:
-                            self.nrcps = int(line.split()[-1])
-                        elif "Number of CCPs  =" in line:
-                            self.nccps = int(line.split()[-1])
-
-                        if GLOBALS.OPTIMISE_ATOM == "all":
-
-                            # Check which .int files should be produced.
-                            # if AIMALL does the partitioning for more than 1 atom, then it does not write out a line with `Out File`
-                            if "aimint.exe" in line:
-                                # find the path to where the atom information will be stored (this is without suffix), also remove extra " in line
-                                aimatom_path = Path(line.split()[-2].strip('"'))
-                                # convert the stem to upper, eg. o1 -> O1
-                                inpfile = aimatom_path.with_suffix(".inp")
-                                outfile = aimatom_path.with_suffix(".int")
-                                atom_name = aimatom_path.stem.upper()
-                                self[atom_name] = AimAtom(
-                                    atom_name=atom_name, inp_file=inpfile, int_file=outfile
-                                )
-                            elif "Total time for atom" in line:
-                                record = line.split()
-                                atom_name = record[4]
-                                self[atom_name].time_taken = int(record[6])
-                                self[atom_name].integration_error = float(record[-1])
-
-                        # if AIMALL only performs partitioning for one atom there are different line in the .aim file
-                        else:
-                            if "Inp File:" in line:
-                                inpfile = Path(line.split()[-1].strip('"'))
-                                outfile = inpfile.with_suffix(".int")
-                                atom_name = inpfile.stem.upper()
-                                self[atom_name] = AimAtom(
-                                    atom_name=atom_name, inp_file=inpfile, int_file=outfile
-                                )
-
-                            elif "Total time for atom" in line:
-                                record = line.split()
-                                atom_name = record[4]
-                                self[atom_name].time_taken = int(record[6])
-                                self[atom_name].integration_error = float(record[-1])
+                    elif "Total time for atom" in line:
+                        record = line.split()
+                        atom_name = record[4]
+                        self[atom_name].time_taken = int(record[6])
+                        self[atom_name].integration_error = float(record[-1])
 
     def __getitem__(self, item: Union[str, int]) -> AimAtom:
         """ If an integer is passed, it returns the atom whose index corresponds to the integer + 1. If a string is passed, it returns
