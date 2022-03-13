68a155289b8d0a5c4998bb72f349c1434ef26d02
m-julian <52214154+m-julian@users.noreply.github.com>
2022-03-13 13:33:38 +0000

    remove check types

--- ichor/globals/globals.py
+++ ichor/globals/globals.py
@@ -358,13 +358,6 @@ class Globals:
         **kwargs,
     ):
 
-        # check types
-        for global_variable in self.global_variables:
-            if global_variable not in self.__annotations__.keys():
-                self.__annotations__[global_variable] = type(
-                    self.get(global_variable)
-                )
-
         # Set Protected Variables
         self._protected = [
             "FILE_STRUCTURE",
