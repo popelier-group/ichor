d735719a478b6988b0cc5543d3b424d3c2b699a4
m-julian <52214154+m-julian@users.noreply.github.com>
2022-03-29 22:11:46 +0100

    add defaults

---
+++ ichor/defaults/gaussian_defaults.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from typing import List
+
+class GaussianJobType(Enum):
+    """Enum that give variable names to some of the keywords used in a Gaussian job."""
+
+    # this is used to print extra things in the Gaussian output file.
+    # since no other job is specified, this defaults to just single point.
+    SinglePointEnergy = "p"
+
+    Optimisation = "opt"
+    Frequency = "freq"
+
+    @classmethod
+    def types(cls) -> List[str]:
+        return [ty.value for ty in GaussianJobType]
+
+job_type = GaussianJobType.SinglePointEnergy
+startup_options = ["nproc=2", "mem=2GB"]
+method = "B3LYP"
+basis_set = "6-31+g(d,p)"
+charge = 0
+multiplicity = 1
+keywords = []
