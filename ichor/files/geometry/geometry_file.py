68a094ec9516ab2c3c9c3e49fee2ff00eef59b8a
m-julian <52214154+m-julian@users.noreply.github.com>
2022-03-29 22:13:03 +0100

    add docstring

--- ichor/files/geometry/geometry_file.py
+++ ichor/files/geometry/geometry_file.py
@@ -5,6 +5,16 @@ from ichor.files.file import File, FileContents
 from ichor.files.geometry.atom_data import AtomData
 
 class GeometryFile(File):
+    """ A class which is inherited from any file which contains the full geometry
+    of the molecule/system. These geometries can be used to calculate connectivity
+    and ALF. Files such as .xyz, .gjf, .wfn have the full geometry of the system
+    (i.e. they have x,y,z coordinates for every atom in the system, but each file
+    might have different units!). We can use any of these files to determine the
+    connectivity or calculate the ALF.
+    
+    :param path: a path to a file
+    """
+
     atoms: Optional[Atoms]
 
     def __init__(self, path):
