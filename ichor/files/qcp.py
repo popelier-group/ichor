5401ea90395e03bc931ab3f58084c0e7cbbebb34
m-julian <52214154+m-julian@users.noreply.github.com>
2022-03-29 22:36:33 +0100

    add atoms and move typings
    * Moved typing to be in the function arguments instead in the actual init method
    * Added atoms here because any GeometryFile needs to have an Atoms instance

--- ichor/files/qcp.py
+++ ichor/files/qcp.py
@@ -2,15 +2,19 @@ from abc import ABC
 from pathlib import Path
 from typing import Optional, Union
 
-from ichor.files.file import File, FileContents
+from ichor.files.file import FileContents
 from ichor.files.geometry import GeometryFile
+from ichor.atoms import Atoms
 
 
 class QuantumChemistryProgramInput(GeometryFile, ABC):
     """Abstract class to interface with quantum chemistry programs"""
 
-    def __init__(self, path: Union[Path, str]):
-        super().__init__(path)
+    def __init__(self, path: Union[Path, str],
+                method: Optional[str] = FileContents,
+                basis_set: Optional[str] = FileContents,
+                atoms: Atoms = FileContents):
+        super().__init__(path, atoms)
 
-        self.method: Optional[str] = FileContents
-        self.basis_set: Optional[str] = FileContents
+        self.method = method
+        self.basis_set = basis_set
