47f1c413ef5d5c0067bcf803f9fed47f95b92583
m-julian <52214154+m-julian@users.noreply.github.com>
2022-04-01 22:39:42 +0100

    simplify coordinate line
    * Not sure why coordinate line had this at the end, it should only match three numbers and nothing else after (except whitespace). Otherwise, it can match other lines in Gaussian as well

--- ichor/patterns.py
+++ ichor/patterns.py
@@ -2,7 +2,7 @@
 
 import re
 
-COORDINATE_LINE = re.compile(r"\s*\w+(\s*[+-]?\d+.\d+([Ee]?[+-]?\d+)?){3}")
+COORDINATE_LINE = re.compile(r"\s*\w+(\s*[+-]?\d+.\d+\s*){3}$")
 AIMALL_LINE = re.compile(r"[<|]?\w+[|>]?\s+=(\s+[+-]?\d+.\d+([Ee]?[+-]?\d+)?)")
 MULTIPOLE_LINE = re.compile(
     r"Q\[\d+,\d+(,\w+)?]\s+=\s+[+-]?\d+.\d+([Ee]?[+-]?\d+)?"
