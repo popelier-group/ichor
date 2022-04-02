8a365ce66dced179053eb93b87bbbfba131c7ae8
m-julian <52214154+m-julian@users.noreply.github.com>
2022-04-02 12:59:30 +0100

    move methods/change __getattribute__

--- ichor/files/file.py
+++ ichor/files/file.py
@@ -1,15 +1,12 @@
-import shutil
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from enum import Enum
 from pathlib import Path
 from typing import List, Optional, Union
 
-from ichor.common.functools import (buildermethod, classproperty)
+from ichor.common.functools import buildermethod, classproperty
 from ichor.common.io import move
-from ichor.common.obj import (object_getattribute)
 from ichor.files.path_object import PathObject
-
 
 class FileReadError(Exception):
     pass
@@ -26,14 +23,19 @@ class FileState(Enum):
 
 
 class FileContentsType:
+    """ A class whose instance is used for class attributes that are read in from a file.
+    If a class attribute is FileContents type, then we read the file and store the read in value.
+    This class allows for lazily reading files (i.e. files are not directly read in when
+    an instance of a File (or its subclasses) is made, but only when attributes of that
+    instance (which are FileContents) are being accessed."""
     pass
 
     def __bool__(self):
+        """ FileContents instances must evaluate to False (as NoneType)."""
         return False
 
-
+# make an instance of FileContentsType which to use everywhere.
 FileContents = FileContentsType()
-
 
 class File(PathObject, ABC):
     """Abstract Base Class for any type of file that is used by ICHOR."""
@@ -52,12 +54,10 @@ class File(PathObject, ABC):
         # but can still construct file from passing in arguments / writing out default arguments
         else:
             super().__init__(path)
-            self.state = FileState.Read
-
-            
+            self.state = FileState.Read            
 
     @buildermethod
-    def read(self, *args, **kwargs) -> None:
+    def read(self, *args, **kwargs):
         """Read the contents of the file. Depending on the type of file, different parts will be read in.
         
         .. note::
@@ -80,6 +80,65 @@ class File(PathObject, ABC):
         different."""
         pass
 
+    @classproperty
+    @abstractmethod
+    def filetype(self) -> str:
+        """ Abstract class property which returns the suffix associated with the filetype.
+        For example, for GJF class, this will return `.gjf`"""
+        pass
+
+    @classmethod
+    def check_path(cls, path: Path) -> bool:
+        """ Checks the suffix of the given path matches the filetype associated with class that subclasses from File
+        :param path: A Path object to check
+        :return: True if the Path object has the same suffix as the class filetype, False otherwise
+        """
+        return cls.filetype == path.suffix
+
+    def __getattribute__(self, item):
+        """This is what gets called when accessing an attribute of an instance. Here, we check if the attribute exists or not.
+        If the attribute does not exist, then read the file and update its filestate. Then try to return the value of the attribute, if
+        the attribute still does not exist after reading the file, then return an AttributeError.
+
+        One must be careful to make sure all attributes that want to be accessed lazily must be an attribute of the class and
+        not to override __getattribute__ in subclasses of PathObject.
+
+        :param item: The attribute that needs to be accessed.
+        """
+
+        # check if the attribute has value FileContents, if not read file
+        try:
+            if super().__getattribute__(item) is FileContents:
+                self.read()
+        except AttributeError:
+            pass
+
+        try:
+            return super().__getattribute__(item)
+        except AttributeError:
+            raise AttributeError(f"{self} does not have attribute {item}.")
+
+    def __getitem__(self, item):
+        """Tries to return the item indexed with [] brackets. If the item does not exist and the filestate is Unread, then
+        read the file and try to access the item again. If the item still does not exist, then throw a KeyError."""
+        try:
+            return super().__getitem__(item)
+        except KeyError:
+            if self.state is FileState.Unread:
+                self.read()
+                return self.__getitem__(item)
+        raise KeyError(f"No '{item}' item found for '{self.path}'")
+
+    def move(self, dst):
+        """Move the file to a new destination.
+
+        :param dst: The new path to the file. If a directory, the file is moved inside the directory.
+        """
+        if dst.is_dir():
+            # pathlib's Path class uses / operator to add to the path
+            dst /= self.path.name
+        move(self.path, dst)
+
     def dump(self):
         """ Sets all attributes in self._contents to FileContents. self._contents
         is a list of strings corresponding to attributes which are initially of type
@@ -89,38 +148,6 @@ class File(PathObject, ABC):
         for var in self._contents:
             setattr(self, var, FileContents)
         self.state = FileState.Unread
-
-    @classproperty
-    @abstractmethod
-    def filetype(self) -> str:
-        """ Abstract class property which returns the suffix associated with the filetype.
-        For example, for GJF class, this will return `.gjf`"""
-        pass
-
-    @property
-    def _file_contents(self):
-        return list(dir(self)) + self.file_contents
-
-    @property
-    def file_contents(self) -> List[str]:
-        return []
-
-    @classmethod
-    def check_path(cls, path: Path) -> bool:
-        """ Checks the suffix of the given path matches the filetype associated with class that subclasses from File
-        :param path: A Path object to check
-        :return: True if the Path object has the same suffix as the class filetype, False otherwise
-        """
-        return path.suffix == cls.filetype
-
-    def move(self, dst) -> None:
-        """Move the file to a new destination.
-
-        :param dst: The new path to the file. If a directory, the file is moved inside the directory.
-        """
-        if dst.is_dir():
-            dst /= self.path.name
-        move(self.path, dst)
 
     def write(self, path: Optional[Path] = None):
         """ This write method should only be called if no other write method exists. A
@@ -145,41 +172,3 @@ class File(PathObject, ABC):
         """ Unblocks a blocked file."""
         if self.state is FileState.Blocked:
             self.state = self._save_state
-
-    def __getattribute__(self, item):
-        """This is what gets called when accessing an attribute of an instance. Here, we check if the attribute exists or not.
-        If the attribute does not exist, then read the file and update its filestate. Then try to return the value of the attribute, if
-        the attribute still does not exist after reading the file, then return an AttributeError.
-
-        One must be careful to make sure all attributes that want to be accessed lazily must be an attribute of the class and
-        not to override __getattribute__ in subclasses of PathObject.
-
-        :param item: The attribute that needs to be accessed.
-        """
-
-        # check if the attribute has value FileContents, if not read file
-        try:
-            if super().__getattribute__(item) is FileContents:
-                self.read()
-        except AttributeError:  # todo: see if we can get rid of the need for this as can cause issues if there is truly an AttributeError in self.read()
-            if item in self._file_contents:
-                self.read()
-
-        # now that the file is read, return the attribute that should exist now
-        try:
-            return super().__getattribute__(item)
-        except AttributeError:
-            raise AttributeError(
-                f"{object_getattribute(self, 'path')} instance of {object_getattribute(self, '__class__').__name__} has no attribute {item}"
-            )
-
-    def __getitem__(self, item):
-        """Tries to return the item indexed with [] brackets. If the item does not exist and the filestate is Unread, then
-        read the file and try to access the item again. If the item still does not exist, then throw a KeyError."""
-        try:
-            return super().__getitem__(item)
-        except KeyError:
-            if self.state is FileState.Unread:
-                self.read()
-                return self.__getitem__(item)
-        raise KeyError(f"No '{item}' item found for '{self.path}'")
