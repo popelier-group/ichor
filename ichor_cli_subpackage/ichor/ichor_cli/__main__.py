f536d3bb93f14c134ae79265ff0ca1e9d15dce9b
m-julian <52214154+m-julian@users.noreply.github.com>
2022-04-30 19:06:57 +0100

    add init and main

---
+++ ichor_cli_subpackage/ichor/ichor_cli/__main__.py
@@ -0,0 +1,9 @@
+from ichor.ichor_cli.menus import main_hpc_menu, main_simple_menu
+from ichor.ichor_hpc.machine_setup.machine import MACHINE, Machine
+
+# if MACHINE is Machine.local:
+#     main_simple_menu()
+# else:
+#     main_hpc_menu()
+
+main_simple_menu()
