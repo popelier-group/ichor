6d4e231a80d447f090a92882443c3caa40f55e78
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-02 18:30:56 +0100

    remove tqdm

--- ichor_lib_subpackage/ichor/ichor_lib/common/types/__init__.py
+++ ichor_lib_subpackage/ichor/ichor_lib/common/types/__init__.py
@@ -1,7 +1,6 @@
 from ichor.ichor_lib.common.types.bool_toggle import BoolToggle
 from ichor.ichor_lib.common.types.class_dict import ClassDict
 from ichor.ichor_lib.common.types.dict_list import DictList
-from ichor.ichor_lib.common.types.dummy_tqdm import dummy_tqdm
 from ichor.ichor_lib.common.types.enum import Enum, EnumStrList
 from ichor.ichor_lib.common.types.file_tree import (FileNode, FileTree, FileType,
                                           WrongFileType)
