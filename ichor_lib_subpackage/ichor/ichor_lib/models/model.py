ebdce95752159281a25db5e6895448639d7c5edc
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-24 09:47:32 +0100

    file reading and init update

--- ichor_lib_subpackage/ichor/ichor_lib/models/model.py
+++ ichor_lib_subpackage/ichor/ichor_lib/models/model.py
@@ -4,7 +4,6 @@ from typing import Dict, List, Optional
 import numpy as np
 
 from ichor.ichor_lib.common.functools import cached_property, classproperty
-from ichor.ichor_lib.common.functools.buildermethod import buildermethod
 from ichor.ichor_lib.common.io import mkdir
 from ichor.ichor_lib.common.str import get_digits
 from ichor.ichor_lib.common.types import Version
@@ -41,53 +40,33 @@ class Model(File):
         Another program can be used for the machine learning as long as it outputs files of the same format as the FEREBUS outputs.
     """
 
-    # these can be accessed with __annotations__, so leave them
-    system: str
-    atom: str
-    type: str
-    alf: List[int]
-    natoms: int
-    nfeats: int
-    ntrain: int
-    mean: Mean
-    kernel: Kernel
-    x: np.ndarray
-    y: np.ndarray
-    input_units: List[str]
-    output_unit: str
-    likelihood: float
-    nugget: float
-    weights: np.ndarray
-    program: str
-    program_version: Version
-    notes: Dict[str, str]
-
     def __init__(self, path: Path,
-            system = FileContents,
-            atom = FileContents,
-            type_ = FileContents,
-            alf = FileContents,
-            natoms = FileContents,
-            ntrain = FileContents,
-            nfeats = FileContents,
-            mean = FileContents,
-            kernel = FileContents,
-            x = FileContents,
-            y = FileContents,
-            input_units = FileContents,
-            output_unit = FileContents,
-            likelihood = FileContents,
-            nugget = FileContents,
-            weights = FileContents,
-            program = FileContents,
-            program_version = FileContents,
-            notes = FileContents
+            system_name: str = FileContents,
+            atom_name: str = FileContents,
+            prop: str = FileContents,
+            alf: List[int] = FileContents,
+            natoms: int = FileContents,
+            ntrain: int = FileContents,
+            nfeats: int = FileContents,
+            mean: Mean = FileContents,
+            kernel: Kernel = FileContents,
+            x: np.ndarray = FileContents,
+            y: np.ndarray = FileContents,
+            input_units: List[str] = FileContents,
+            output_unit: str = FileContents,
+            likelihood: float = FileContents,
+            jitter: float = FileContents,
+            weights: np.ndarray = FileContents,
+            program: str = FileContents,
+            program_version: Version = FileContents,
+            notes: Dict[str, str] = FileContents
     ):
         File.__init__(self, path)
 
-        self.system = system
-        self.atom = atom
-        self.type = type_
+        self.program = program
+        self.system_name = system_name
+        self.atom_name = atom_name
+        self.prop = prop
         self.alf = alf
         self.natoms = natoms
         self.nfeats = nfeats
@@ -99,108 +78,92 @@ class Model(File):
         self.input_units = input_units
         self.output_unit = output_unit
         self.likelihood = likelihood
-        self.nugget = nugget
+        self.jitter = jitter
         self.weights = weights
-        self.program = program
         self.program_version = program_version
         self.notes = notes
 
-    @buildermethod
-    def _read_file(self, up_to: Optional[str] = None) -> None:
-        try:
-            self.__read_file(up_to)
-        except:
-            raise ValueError(f"Error reading model file: {self.path}")
-
-    def __read_file(self, up_to: Optional[str] = None) -> None:
+    def _read_file(self):
         """Read in a FEREBUS output file which contains the optimized hyperparameters, mean function, and other information that is needed to make predictions."""
         kernel_composition = ""
-        kernel_list = {}
-
-        self.notes = {}
-        self.nugget = 1e-10
-
-        stopiter = False
+        kernel_dict = {}
+        notes = {}
 
         with open(self.path) as f:
             for line in f:
-                if stopiter:
-                    break
-
-                if up_to is not None and up_to in line:
-                    stopiter = True
 
                 if "<TODO>" in line:
                     continue
 
                 if "program" in line:
-                    self.program = line.split()[-1]
+                    self.program = self.program or line.split()[-1]
                     continue
 
                 if "version" in line:
-                    self.program_version = Version(line.split()[-1])
+                    self.version = self.version or Version(line.split()[-1])
                     continue
 
                 if (
-                    "nugget" in line
+                    "jitter" in line
                 ):  # noise to add to the diagonal to help with numerical stability. Typically on the scale 1e-6 to 1e-10
-                    self.nugget = float(line.split()[-1])
+                    self.jitter = self.jitter or float(line.split()[-1])
                     continue
 
                 if "likelihood" in line:
-                    self.likelihood = float(line.split()[-1])
+                    self.likelihood = self.likelihood or float(line.split()[-1])
                     continue
 
                 if "#" in line and "=" in line:
                     line = line.lstrip("#")
                     key, val = line.split("=")
-                    self.notes[key.strip()] = val.strip()
+                    notes[key.strip()] = val.strip()
                     continue
 
                 if line.startswith("#"):
-                    continue
+                    line = line.lstrip("#")
+                    notes[line.strip()] = None
 
                 if "name" in line:  # system name e.g. WATER
-                    self.system = line.split()[1]
+                    self.system_name = self.system_name or line.split()[1]
                     continue
 
                 if line.startswith(
                     "atom"
                 ):  # atom for which a GP model was made eg. O1
-                    self.atom = line.split()[1]
+                    self.atom_name = self.atom_name or line.split()[1]
                     continue
 
                 if (
                     "property" in line
                 ):  # property (such as iqa or particular multipole moment) for which a GP model was made
-                    self.type = line.split()[1]
+                    self.prop = self.prop or line.split()[1]
                     continue
 
                 if "ALF" in line:
-                    self.alf = [int(a) for a in line.split()[1:]]
+                    self.alf = self.alf or [int(a) for a in line.split()[1:]]
                     continue
 
                 if "number_of_atoms" in line:
-                    self.natoms = int(line.split()[1])
+                    self.natoms = self.natoms or int(line.split()[1])
                     continue
 
                 if "number_of_features" in line:  # number of inputs to the GP
-                    self.nfeats = int(line.split()[1])
+                    self.nfeats = self.nfeats or int(line.split()[1])
                     continue
 
                 if (
                     "number_of_training_points" in line
                 ):  # number of training points to make the GP model
-                    self.ntrain = int(line.split()[1])
+                    self.ntrain = self.ntrain or int(line.split()[1])
                     continue
 
                 # GP mean (mu) section
                 if "[mean]" in line:
                     mean_type = next(f).split()[-1]  # type
                     if mean_type == "constant":
-                        self.mean = ConstantMean(float(next(f).split()[1]))
+                        mean = ConstantMean(float(next(f).split()[1]))
                     elif mean_type == "zero":
-                        self.mean = ZeroMean()
+                        mean = ZeroMean()
                     elif mean_type in ["linear", "quadratic"]:
                         beta = np.array(
                             [float(b) for b in next(f).split()[1:]]
@@ -210,9 +173,11 @@ class Model(File):
                         )
                         ymin = float(next(f).split()[-1])
                         if mean_type == "linear":
-                            self.mean = LinearMean(beta, xmin, ymin)
+                            mean = LinearMean(beta, xmin, ymin)
                         elif mean_type == "quadratic":
-                            self.mean = QuadraticMean(beta, xmin, ymin)
+                            mean = QuadraticMean(beta, xmin, ymin)
+
+                    self.mean = self.mean or mean
                     continue
 
                 if (
@@ -239,7 +204,7 @@ class Model(File):
                         thetas = np.array(
                             [float(hp) for hp in next(f).split()[1:]]
                         )
-                        kernel_list[kernel_name] = RBF(
+                        kernel_dict[kernel_name] = RBF(
                             kernel_name, thetas, active_dims=active_dims
                         )
                     elif kernel_type in [
@@ -249,80 +214,76 @@ class Model(File):
                         thetas = np.array(
                             [float(hp) for hp in next(f).split()[1:]]
                         )
-                        kernel_list[kernel_name] = RBFCyclic(
+                        kernel_dict[kernel_name] = RBFCyclic(
                             kernel_name, thetas, active_dims=active_dims
                         )
                     elif kernel_type == "constant":
                         value = float(next(f).split()[-1])
-                        kernel_list[kernel_name] = ConstantKernel(
+                        kernel_dict[kernel_name] = ConstantKernel(
                             kernel_name, value, active_dims=active_dims
                         )
                     elif kernel_type == "periodic":
                         thetas = np.array(
                             [float(hp) for hp in next(f).split()[1:]]
                         )
-                        kernel_list[kernel_name] = PeriodicKernel(
+                        kernel_dict[kernel_name] = PeriodicKernel(
                             kernel_name,
                             thetas,
                             np.full(thetas.shape, 2 * np.pi),
                             active_dims=active_dims,
                         )
+
+                        self.kernel = self.kernel or KernelInterpreter(
+                            kernel_composition, kernel_dict
+                        ).interpret()
+
                     continue
 
                 if "units.x" in line:
-                    self.input_units = line.split()[1:]
+                    self.input_units = self.input_units or line.split()[1:]
 
                 if "units.y" in line:
-                    self.output_unit = line.split()[-1]
+                    self.output_unit = self.output_unit or line.split()[-1]
 
                 # training inputs data
                 if "[training_data.x]" in line:
                     line = next(f)
-                    self.x = np.empty((self.ntrain, self.nfeats))
+                    x = np.empty((self.ntrain, self.nfeats))
                     i = 0
                     while line.strip() != "":
-                        self.x[i, :] = np.array(
+                        x[i, :] = np.array(
                             [float(num) for num in line.split()]
                         )
                         i += 1
                         line = next(f)
+                    self.x = self.x or x
                     continue
 
                 # training labels data
                 if "[training_data.y]" in line:
                     line = next(f)
-                    self.y = np.empty((self.ntrain, 1))
+                    y = np.empty((self.ntrain, 1))
                     i = 0
                     while line.strip() != "":
                         self.y[i, 0] = float(line)
                         i += 1
                         line = next(f)
+                    self.y = self.y or y
                     continue
 
                 if "[weights]" in line:
                     line = next(f)
-                    self.weights = np.empty((self.ntrain, 1))
+                    weights = np.empty((self.ntrain, 1))
                     i = 0
                     while line.strip() != "":
-                        self.weights[i, 0] = float(line)
+                        weights[i, 0] = float(line)
                         i += 1
                         try:
                             line = next(f)
                         except:
                             break
 
-        if kernel_composition:
-            self.kernel = KernelInterpreter(
-                kernel_composition, kernel_list
-            ).interpret()
-
-    @property
-    def atom_name(self) -> str:
-        return self.atom
-
-    @atom_name.setter
-    def atom_name(self, value: str):
-        self.atom = value
+                    self.weights = self.weights or weights
 
     @classproperty
     def filetype(self) -> str:
@@ -331,12 +292,16 @@ class Model(File):
 
     @property
     def ialf(self) -> np.ndarray:
+        """ Returns the atomic local frame, indices start at 0 (as in Python).
+        
+        :return: The 0-indexed np.ndarray corresponding to the alf of the atom.
+        """
         return np.array(self.alf) - 1
 
     @property
     def atom_num(self) -> int:
         """Returns the integer that is in the atom name"""
-        return get_digits(self.atom)
+        return get_digits(self.atom_name)
 
     @property
     def i(self) -> int:
@@ -344,31 +309,32 @@ class Model(File):
         This is the index of the atom in Python objects such as lists (as indeces start at 0)."""
         return self.atom_num - 1
 
+    @property
     def r(self, x_test: np.ndarray) -> np.ndarray:
         """Returns the n_train by n_test covariance matrix"""
         return self.kernel.r(self.x, x_test)
 
-    @cached_property
+    @property
     def R(self) -> np.ndarray:
         """Returns the covariance matrix and adds a jitter to the diagonal for numerical stability. This jitter is a very
         small number on the order of 1e-6 to 1e-10."""
-        return self.kernel.R(self.x) + (self.nugget * np.identity(self.ntrain))
-
-    @cached_property
+        return self.kernel.R(self.x) + (self.jitter * np.identity(self.ntrain))
+    
+    @property
     def invR(self) -> np.ndarray:
         """Returns the inverse of the covariance matrix R"""
         return np.linalg.inv(self.R)
 
-    @cached_property
+    @property
     def lower_cholesky(self) -> np.ndarray:
         """Decomposes the covariance matrix into L and L^T. Returns the lower triangular matrix L."""
         return np.linalg.cholesky(self.R)
 
-    @cached_property
+    @property
     def _y_minus_mean(self):
         return self.y - self.mean.value(self.x).reshape((-1, 1))
 
-    @cached_property
+    @property
     def logdet(self):
         sign, logdet = np.linalg.slogdet(self.R)
         return sign * logdet
@@ -402,107 +368,30 @@ class Model(File):
         # here it can only be used to compare points to figure out which point has the largest variance.
         return 1.0 - np.diag(np.matmul(v.T, v)).flatten()
 
-    def validate(self):
-        from ichor import __version__
-
-        self.program = (
-            self.program if self.program is not FileContents else "ichor"
-        )
-        self.program_version = (
-            self.program_version
-            if self.program_version is not FileContents
-            else __version__
-        )
-        self.nugget = self.nugget if self.nugget is not FileContents else 1e-10
-
-        self.system = (
-            self.system
-            if self.system is not FileContents
-            else "default_system_name")
-        self.atom = self.atom if self.atom is not FileContents else "X1"
-        self.type = self.type if self.type is not FileContents else "p1"
-        self.alf = self.alf if self.alf is not FileContents else [1, 1, 1]
-
-        if self.x is not FileContents:
-            if self.x.ndim == 1:
-                self.x = self.x.reshape(1, -1)
-            elif self.x.ndim != 2:
-                raise ValueError(
-                    f"Training Input (x) must be 2D, {self.x.ndim}D array encountered"
-                )
-            if self.nfeats is FileContents:
-                self.nfeats = self.x.shape[1]
-            if self.ntrain is FileContents:
-                self.ntrain = self.x.shape[0]
-
-        if self.nfeats is FileContents and self.natoms is not FileContents:
-            self.nfeats = 3 * self.natoms - 6
-
-        if self.natoms is FileContents and self.nfeats is not FileContents:
-            self.natoms = (self.nfeats + 6) // 3
-
-        if self.natoms is FileContents:
-            self.natoms = 1
-
-        if self.nfeats is FileContents:
-            self.nfeats = 0
-
-        if self.ntrain is FileContents:
-            self.ntrain = 0
-
-        if self.x is FileContents:
-            self.x = np.zeros((self.ntrain, self.nfeats))
-
-        if self.y is FileContents:
-            self.y = np.zeros((self.ntrain, 1))
-
-        if self.input_units is FileContents:
-            self.input_units = _get_default_input_units(self.nfeats)
-
-        if self.output_unit is FileContents:
-            self.output_unit = _get_default_output_unit(self.type)
-
-        if self.kernel is FileContents:
-            self.kernel = RBFCyclic("k1", np.ones(self.nfeats))
-
-        if self.mean is FileContents:
-            if self.ntrain == 0:
-                self.mean = ConstantMean(0.0)
-            else:
-                self.mean = ConstantMean(np.mean(self.y.flatten()))
-
-        if self.likelihood is FileContents:
-            self.likelihood = self.compute_likelihood()
-
-        if self.weights is FileContents:
-            self.weights = self.compute_weights()
-
     def write(self, path: Optional[Path] = None) -> None:
+
         path = path or self.path
 
         if not path.parent.exists():
             mkdir(path.parent)
         if path.is_dir():
             path = (
-                path / f"{self.system}_{self.type}_{self.atom}{Model.filetype}"
+                path / f"{self.system_name}_{self.prop}_{self.atom_name}{Model.filetype}"
             )
-
-        with self.block():
-            self.validate()
 
         with open(path, "w") as f:
             f.write("# [metadata]\n")
             f.write(f"# program {self.program}\n")
             f.write(f"# version {self.program_version}\n")
-            f.write(f"# nugget {self.nugget}\n")
+            f.write(f"# jitter {self.jitter}\n")
             f.write(f"# likelihood {self.likelihood}\n")
             for key, val in self.notes.items():
                 f.write(f"# {key} = {val}\n")
             f.write("\n")
             f.write("[system]\n")
-            f.write(f"name {self.system}\n")
-            f.write(f"atom {self.atom}\n")
-            f.write(f"property {self.type}\n")
+            f.write(f"name {self.system_name}\n")
+            f.write(f"atom {self.atom_name}\n")
+            f.write(f"property {self.prop}\n")
             f.write(f"ALF {self.alf[0]} {self.alf[1]} {self.alf[2]}\n")
             f.write("\n")
             f.write("[dimensions]\n")
@@ -536,4 +425,4 @@ class Model(File):
             f.write("\n")
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(system={self.system}, atom={self.atom}, type={self.type})"
+        return f"{self.__class__.__name__}(system={self.system_name}, atom={self.atom_name}, type={self.prop})"
