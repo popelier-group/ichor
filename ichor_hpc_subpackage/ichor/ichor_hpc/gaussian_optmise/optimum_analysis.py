c920ea37a04476bc846c4e8794a0112a621df3a7
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-25 14:53:10 +0100

    remove globals

--- ichor_hpc_subpackage/ichor/ichor_hpc/gaussian_optmise/optimum_analysis.py
+++ ichor_hpc_subpackage/ichor/ichor_hpc/gaussian_optmise/optimum_analysis.py
@@ -7,50 +7,20 @@ from ichor.ichor_lib.files import WFN
 class UnknownOptimumEnergyFileType(Exception):
     pass
 
-
 def get_wfn_energy_from_dir(d: Path) -> Optional[float]:
     wfns = get_files_of_type(WFN.filetype, d)
     if len(wfns) > 0:
         return WFN(wfns[0]).energy
     return None
 
-
-def find_opt() -> Optional[float]:
+def get_wfn_energy_from_file(wfn_file: Path) -> Optional[float]:
     """
     Tries to find the optimum energy for the current system
     :return: ether the optimum energy or None if the optimum energy cannot be found
     :raises: UnknownOptimumEnergyFileType if GLOBALS.OPTIMUM_ENERGY_FILE is provided but is of unknown type
     """
-    if GLOBALS.OPTIMUM_ENERGY is not None:
-        return GLOBALS.OPTIMUM_ENERGY
-    if (
-        GLOBALS.OPTIMUM_ENERGY_FILE is not None
-        and GLOBALS.OPTIMUM_ENERGY_FILE.exists()
-    ):
-        if GLOBALS.OPTIMUM_ENERGY_FILE.is_dir():
-            if (
-                GLOBALS.OPTIMUM_ENERGY_FILE / FILE_STRUCTURE["opt"]
-            ).exists():  # ichor directory
-                wfn_energy = get_wfn_energy_from_dir(
-                    GLOBALS.OPTIMUM_ENERGY_FILE / FILE_STRUCTURE["opt"]
-                )
-                if wfn_energy is not None:
-                    return wfn_energy
-            wfn_energy = get_wfn_energy_from_dir(
-                GLOBALS.OPTIMUM_ENERGY_FILE
-            )  # search directory for wfn
-            if wfn_energy is not None:
-                return wfn_energy
-        elif (
-            GLOBALS.OPTIMUM_ENERGY_FILE.is_file()
-            and GLOBALS.OPTIMUM_ENERGY_FILE.suffix == WFN.filetype
-        ):
-            return WFN(GLOBALS.OPTIMUM_ENERGY_FILE).energy
-        raise UnknownOptimumEnergyFileType(
-            f"Unknown filetype for optimum energy file {GLOBALS.OPTIMUM_ENERGY_FILE}, must be of type {WFN.filetype}"
-        )
-    if FILE_STRUCTURE["opt"].exists():
-        wfn_energy = get_wfn_energy_from_dir(FILE_STRUCTURE["opt"])
+
+    if wfn_file.exists():
+        wfn_energy = WFN(wfn_file).energy
         if wfn_energy is not None:
             return wfn_energy
-    return None
