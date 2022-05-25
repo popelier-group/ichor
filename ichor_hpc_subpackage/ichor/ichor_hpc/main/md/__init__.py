9b1d90dfe298f717120c661168b8ebcb5fac5356
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-25 15:48:12 +0100

    remove the md menu from hpc

--- ichor_hpc_subpackage/ichor/ichor_hpc/main/md/__init__.py
+++
@@ -1,4 +0,0 @@
-from ichor.main.md.amber import mdcrd_to_xyz, submit_amber
-from ichor.main.md.cp2k import submit_cp2k
-from ichor.main.md.md_menu import md_menu
-from ichor.main.md.tyche import submit_tyche
