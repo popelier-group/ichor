74ef196dfcd6c01de9530046e3b745a1b805acea
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-25 13:17:07 +0100

    add imports

--- ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
+++ ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
@@ -2,3 +2,4 @@ from ichor.ichor_hpc.batch_system.batch_system import Job, JobID
 from ichor.ichor_hpc.batch_system.local import LocalBatchSystem
 from ichor.ichor_hpc.batch_system.node import NodeType
 from ichor.ichor_hpc.batch_system.sge import SunGridEngine
+from ichor.ichor_hpc.batch_system.parallel_environment import ParallelEnvironment, ParallelEnvironments
