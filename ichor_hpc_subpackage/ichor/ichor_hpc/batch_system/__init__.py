b05d96c05b0d9e23cfc07937809d989191795110
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-25 12:44:05 +0100

    remove unused import

--- ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
+++ ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
@@ -1,7 +1,6 @@
 from ichor.ichor_hpc.batch_system.batch_system import Job, JobID
 from ichor.ichor_hpc.batch_system.local import LocalBatchSystem
 from ichor.ichor_hpc.batch_system.node import NodeType
-from ichor.ichor_hpc.batch_system.parallel_environment import ParallelEnvironments
 from ichor.ichor_hpc.batch_system.sge import SunGridEngine
 
 __all__ = ["Job", "JobID", "NodeType", "BATCH_SYSTEM"]
