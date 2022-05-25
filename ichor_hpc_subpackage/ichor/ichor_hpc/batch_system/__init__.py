66d459f9805b6f0b0cbbf5237b35b1404d349b95
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-25 14:41:22 +0100

    add import

--- ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
+++ ichor_hpc_subpackage/ichor/ichor_hpc/batch_system/__init__.py
@@ -3,3 +3,4 @@ from ichor.ichor_hpc.batch_system.local import LocalBatchSystem
 from ichor.ichor_hpc.batch_system.node import NodeType
 from ichor.ichor_hpc.batch_system.sge import SunGridEngine
 from ichor.ichor_hpc.batch_system.parallel_environment import ParallelEnvironment, ParallelEnvironments
+from ichor.ichor_hpc.batch_system.machine_setup import Machine
