e0ce3911ab47015084a18c9e90523e10418c0fed
m-julian <52214154+m-julian@users.noreply.github.com>
2022-05-29 21:14:29 +0100

    add new calculators

--- ichor_lib_subpackage/ichor/ichor_lib/atoms/calculators/feature_calculator/__init__.py
+++ ichor_lib_subpackage/ichor/ichor_lib/atoms/calculators/feature_calculator/__init__.py
@@ -1,3 +1,4 @@
 from ichor.ichor_lib.atoms.calculators.feature_calculator.alf_feature_calculator import (
-    ALFFeatureCalculator,
+    ALFFeatureCalculator, 
 )
+from ichor.ichor_lib.atoms.calculators.feature_calculator.feature_calculator import FeatureCalculator
