b0b1ba7caba309b982ce8c8cac072a6c0b9ba502
m-julian <52214154+m-julian@users.noreply.github.com>
2022-06-09 22:28:59 +0100

    Update setup.py

--- ichor_core_subpackage/setup.py
+++ ichor_core_subpackage/setup.py
@@ -1,16 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-setup(
-    name='ichor-core',
-    author="Matthew Burn, Yulian Manchev",
-    author_email="matthew.burn@postgrad.manchester.ac.uk, yulian.manchev@postgrad.manchester.ac.uk",
-    project_urls = {"github": "https://github.com/popelier-group/ICHOR"},
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent",
-    ],
-    description="ICHOR core subpackage.",
-    packages=find_namespace_packages(include=['ichor*']),
-    python_requires=">=3.7",
-    install_requires=["pandas"]
-)
+if __name__ == "__main__":
+    setup()
