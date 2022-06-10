3f43cc823d2588aaec6f9a8891ad2121568e9f0a
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2022-06-10 14:23:56 +0100

    Added some docs

--- ichor_core_subpackage/ichor/core/common/formatting.py
+++ ichor_core_subpackage/ichor/core/common/formatting.py
@@ -3,10 +3,33 @@ from ichor.core.units import Temperature
 
 
 def format_number_with_comma(n: Scalar) -> str:
+    """
+    10000 -> '10,000'
+
+    Parameters
+    ----------
+    n
+
+    Returns
+    -------
+
+    """
     return f"{n,}"
 
 
 def temperature_formatter(
     temperature: float, unit: Temperature = Temperature.Kelvin
 ) -> str:
+    """
+    100 -> '100 K'
+
+    Parameters
+    ----------
+    temperature
+    unit
+
+    Returns
+    -------
+
+    """
     return f"{temperature} {unit.name}"
