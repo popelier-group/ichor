00a7598ac2971b5e440974ac92c2066b31b365c0
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2022-06-29 11:34:18 +0100

    Improved globals docs

--- gen_globals_docs.py
+++ gen_globals_docs.py
@@ -1,14 +1,38 @@
+import inspect
+from contextlib import suppress
+from typing import Optional
+
 from ichor.hpc.globals import Globals
 
-with open("globals.md", "w") as f:
-    globals = Globals()
-    f.write("| Name | Type | Default Value |\n")
-    f.write("| --- | --- | --- |\n")
-    for global_variable in globals._global_variables:
-        try:
-            if global_variable not in globals._protected:
-                f.write(
-                    f"| {global_variable} | {globals.__annotations__[global_variable].__name__} | {globals._defaults[global_variable]} |\n"
-                )
-        except:
-            pass
+
+def get_note(variable_name: str, source_code: str) -> Optional[str]:
+    for line in source_code.split("\n"):
+        if "def __init__" in line:
+            break
+        if f"{variable_name}:" in line and "#" in line:
+            return line.split("#")[-1]
+
+
+if __name__ == "__main__":
+    with open("globals.md", "w") as f:
+        globals = Globals()
+        globals_source = inspect.getsource(Globals)
+        f.write("| Name | Type | Default Value | Notes |\n")
+        f.write("| --- | --- | --- | --- |\n")
+        for global_variable in globals._global_variables:
+            with suppress(KeyError):
+                if global_variable not in globals._protected:
+                    ty = None
+                    global_type = globals.__annotations__[global_variable]
+                    try:
+                        ty = global_type.__name__
+                    except AttributeError:
+                        if hasattr(global_type, "__bases__"):
+                            ty = global_type.__bases__.replace("typing.", "")
+                        else:
+                            ty = str(global_type)
+                    note = get_note(global_variable, globals_source)
+                    note = note.strip() if note is not None else ""
+                    f.write(
+                        f"| {global_variable} | {global_type} | {globals._defaults[global_variable]} | {note} |\n"
+                    )
