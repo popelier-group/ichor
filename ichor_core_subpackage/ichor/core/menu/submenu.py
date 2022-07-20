ef3539495a4f3fd12ab6279f386e6bce88321e28
MattBurn <matthew.burn@postgrad.manchester.ac.uk>
2022-07-20 17:21:09 +0100

    should now work on windows

--- ichor_core_subpackage/ichor/core/menu/submenu.py
+++ ichor_core_subpackage/ichor/core/menu/submenu.py
@@ -1,10 +1,11 @@
+import os
 from pathlib import Path
-from typing import Any, Sequence
+from typing import Any, List, Sequence
 
 from ichor.core.common.os import input_with_prefill
 from ichor.core.itypes import Scalar, T
 from ichor.core.menu.menu import MenuVar, make_title
-from ichor.core.menu.simple_term_menu import TerminalMenu
+from ichor.core.menu.tab_completer import ListCompleter
 
 # todo: document
 
@@ -30,12 +31,14 @@ def return_arg(arg: T) -> T:
     return arg
 
 
-def select_multiple_from_list(
+def select_multiple_from_list_posix(
     items: Sequence[T],
     result: MenuVar[Sequence[T]],
     title="Select Items",
     empty_ok=False,
 ):
+    from ichor.core.menu.simple_term_menu import TerminalMenu
+
     menu = TerminalMenu(
         items,
         title=make_title(title),
@@ -52,6 +55,48 @@ def select_multiple_from_list(
         result.var = type(result.var)(map(type(menu_result[0]), menu_result))
     else:
         result.var = type(result.var)()
+
+
+def select_multiple_from_list_compatability(
+    items: List[T],
+    result: MenuVar[List[T]],
+    title: str = "Select Items",
+    empty_ok: bool = False,
+):
+    exit_menu = False
+    while not exit_menu:
+        print(title)
+        print()
+        for item in items:
+            print(f"[{'x' if item in result.var else ' '}] {item}")
+        print()
+        with ListCompleter(items):
+            while True:
+                ans = input(">> ")
+                if ans == "all":
+                    result.var = list(items)
+                    break
+                elif ans == "clear":
+                    result.var = []
+                    break
+                elif ans in items:
+                    if ans in result.var:
+                        del result.var[result.var.index(ans)]
+                    else:
+                        result.var.append(ans)
+                elif ans.strip() == "" or ans == "exit":
+                    if not empty_ok and len(result.var) == 0:
+                        print("Result cannot be empty")
+                    else:
+                        exit_menu = True
+                        break
+
+
+select_multiple_from_list = (
+    select_multiple_from_list_compatability
+    if os.name == "nt"
+    else select_multiple_from_list_posix
+)
 
 
 def set_number(n: MenuVar[Scalar]):
