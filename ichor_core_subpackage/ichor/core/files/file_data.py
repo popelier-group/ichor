9221a21f52a04f57e31012bf2315babd766318d0
m-julian <52214154+m-julian@users.noreply.github.com>
2022-07-25 16:11:18 +0300

    add centering on atom and properties

--- ichor_core_subpackage/ichor/core/files/file_data.py
+++ ichor_core_subpackage/ichor/core/files/file_data.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Union
+from pathlib import Path
 from abc import ABC
 from typing import List, Callable
 import numpy as np
@@ -24,6 +25,14 @@ class HasAtoms(ABC):
     @property
     def atom_names(self) -> List[str]:
         return [atom.name for atom in self.atoms]
+    
+    @property
+    def natoms(self) -> int:
+        return len(self.atom_names)
+    
+    @property
+    def types_extended(self) -> List[str]:
+        return self.atoms.types_extended
 
     def connectivity(self, connectivity_calculator: Callable[..., np.ndarray]) -> np.ndarray:
         """Return the connectivity matrix (n_atoms x n_atoms) for the given Atoms instance.
@@ -73,6 +82,94 @@ class HasAtoms(ABC):
         """
 
         return {atom_instance.name: atom_instance.features(feature_calculator, *args, **kwargs) for atom_instance in self.atoms}
+
+    def center_geometry_on_atom_and_write_xyz(
+        self,
+        central_atom_alf: ALF,
+        central_atom_name: str,
+        fname: Optional[Union[str, Path]] = None,
+        **kwargs
+    ):
+        """Centers all geometries (from a Trajectory of PointsDirectory instance) onto a central atom and then writes out a new
+        xyz file with all geometries centered on that atom. This is essentially what the ALFVisualizier application (ALFi) does.
+        The features for the central atom are calculated, after which they are converted back into xyz coordinates (thus all geometries)
+        are now centered on the given central atom).
+
+        :param feature_calculator: Function which calculates features
+        :param central_atom_name: the name of the central atom to center all geometries on. Eg. `O1`
+        :param fname: Optional file name in which to save the rotated geometries.
+        :param *args: Positional arguments to pass to calculator function
+        :param **kwargs: Key word arguments to pass to calculator function
+        """
+
+        from ichor.core.atoms import Atoms
+        from ichor.core.files import XYZ
+        from ichor.core.files.trajectory import features_to_coordinates
+        from ichor.core.calculators import calculate_alf_features
+        from ichor.core.units import AtomicDistance
+
+        if central_atom_name not in self.atom_names:
+            raise ValueError(
+                f"Central atom name {central_atom_name} not found in atom names:{self.atom_names}."
+            )
+
+        if not fname:
+            fname = f"{central_atom_name}_centered_geometries.xyz"
+            fname = Path(fname)
+        else:
+            fname = Path(fname)
+            fname = fname.with_suffix(".xyz")
+
+        # before, ordering is 0,1,2,3,4,5,...,etc.
+        # after calculating the features and converting back, the order is going to be
+        # central atom idx, x-axis atom index, xy-plane atom index, rest of atom indices
+        n_atoms = len(self.atom_names)
+        
+        previous_atom_ordering = list(range(n_atoms))
+        current_atom_ordering = list(central_atom_alf) + [
+            i for i in range(n_atoms) if i not in central_atom_alf
+        ]
+        # this will get the index that the atom was moved to after reordering.
+        reverse_alf_ordering = [
+            current_atom_ordering.index(num) for num in range(n_atoms)
+        ]
+        # order will always be central atom(0,0,0), x-axis atom, xy-plane atom, etc.
+        
+        central_atom_features = self.atoms[central_atom_name].features(calculate_alf_features, central_atom_alf)
+        xyz_array = features_to_coordinates(central_atom_features)
+
+        # reverse the ordering, so that the rows are the same as before
+        # can now use the atom names as they were read in in initial Trajectory/PointsDirectory instance.
+        xyz_array[:, previous_atom_ordering, :] = xyz_array[
+            :, reverse_alf_ordering, :
+        ]
+        
+        # there is only one geometry here
+        xyz_array = xyz_array[0]
+        trajectory = XYZ(fname)
+        
+        # initialize empty Atoms instance
+        atoms = Atoms()
+        for ty, atom_coord in zip(self.types_extended, xyz_array):
+            # add Atom instances for every atom in the geometry to the Atoms instance
+            atoms.add(
+                Atom(
+                    ty,
+                    atom_coord[0],
+                    atom_coord[1],
+                    atom_coord[2],
+                    units=AtomicDistance.Bohr,
+                )
+            )
+        # Add the filled Atoms instance to the Trajectory instance and repeat for next geometry
+        atoms = atoms.to_angstroms()
+        trajectory.atoms = atoms
+
+        trajectory.write()
+
+
+
+
 
     def __getitem__(self, s: str):
         if isinstance(s, str):
