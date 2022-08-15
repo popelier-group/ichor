dea6b910b943a9bfbfffbebf0644ad870f59693f
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-15 16:20:14 +0300

    add typing

--- ichor_core_subpackage/ichor/core/calculators/features/alf_features.py
+++ ichor_core_subpackage/ichor/core/calculators/features/alf_features.py
@@ -3,18 +3,14 @@ from ichor.core.calculators.alf import ALF, get_atom_alf
 from ichor.core.calculators.c_matrix_calculator import calculate_c_matrix
 from ichor.core.constants import ang2bohr
 from ichor.core.units import AtomicDistance
-from typing import Union, List
-
-
-
-
+from typing import Union, List, Dict
 
 
 default_distance_unit: AtomicDistance = AtomicDistance.Bohr
 
 def calculate_alf_features(
     atom: "Atom",
-    alf: Union[ALF, List[ALF], List[List[int]]],
+    alf: Union[ALF, List[ALF], List[List[int]], Dict[str, ALF]],
     distance_unit: AtomicDistance = default_distance_unit,
 ) -> np.ndarray:
     """Calculates the features for the given central atom.
