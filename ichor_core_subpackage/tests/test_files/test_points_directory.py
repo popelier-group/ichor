b11c4ddb4a6a4b4ac5856d192e28ffb766cb5476
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-17 16:21:17 +0300

    working on testing points dir

--- ichor_core_subpackage/tests/test_files/test_points_directory.py
+++ ichor_core_subpackage/tests/test_files/test_points_directory.py
@@ -1,3 +1,4 @@
 from tests.test_files.test_read_gjf import _test_read_gjf
 from tests.test_files.test_read_int import _test_int
+from tests.test_files.test_read_wfn import _test_read_wfn, _test_molecular_orbitals
 from tests.test_atoms import _test_atoms_coords
