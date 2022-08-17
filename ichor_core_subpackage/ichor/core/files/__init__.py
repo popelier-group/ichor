4204910b4c63b75415bbbeaaa8aec3b054fafbd0
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-17 11:05:47 +0300

    fix imports

--- ichor_core_subpackage/ichor/core/files/__init__.py
+++ ichor_core_subpackage/ichor/core/files/__init__.py
@@ -1,12 +1,11 @@
 # aimall files
 from ichor.core.files.aimall import AIM, INT, INTs
-from ichor.core.files.aimall import INT
-from ichor.core.files.aimall import INTs
 
 # gaussian files
-from ichor.core.files.gaussian import GJF
-from ichor.core.files.gaussian import WFN
-from ichor.core.files.gaussian import GaussianOut
+from ichor.core.files.gaussian import GJF, WFN, GaussianOut
+
+# pandora files
+from ichor.core.files.pandora import MorfiDirectory, PandoraDirectory, PandoraInput, PySCFDirectory
 
 from ichor.core.files.directory import Directory
 from ichor.core.files.dl_poly import DlpolyHistory, DlPolyField, DlPolyConfig, DlPolyControl
@@ -21,12 +20,6 @@ from ichor.core.files.file_data import HasAtoms, HasProperties
 
 from ichor.core.files.mol2 import Mol2
 from ichor.core.files.optional_file import OptionalFile, OptionalPath
-from ichor.core.files.pandora import (
-    MorfiDirectory,
-    PandoraDirectory,
-    PandoraInput,
-    PySCFDirectory,
-)
 from ichor.core.files.point_directory import PointDirectory
 from ichor.core.files.points_directory import PointsDirectory
 from ichor.core.files.qcp import QuantumChemistryProgramInput
