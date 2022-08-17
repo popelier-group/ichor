c41f99078b4cfd7dd572b105c8c8c1f8fcf8cd71
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-17 09:20:13 +0300

    import update

--- ichor_core_subpackage/ichor/core/atoms/__init__.py
+++ ichor_core_subpackage/ichor/core/atoms/__init__.py
@@ -5,4 +5,5 @@ from ichor.core.atoms.atom import Atom, ConnectedAtom
 from ichor.core.atoms.atoms import AtomNotFound, Atoms, AtomsNotFoundError, ConnectedAtoms
 from ichor.core.atoms.list_of_atoms import ListOfAtoms
 from ichor.core.atoms.alf import ALF
+from ichor.core.atoms.list_of_atoms_atom_view import AtomView
 
