6224b8a84c7eab9200c8ed965a1fd22a2b58a660
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-21 16:14:16 +0300

    add INT AB interaction class

---
+++ ichor_core_subpackage/ichor/core/files/aimall/int_ab.py
@@ -0,0 +1,70 @@
+
+from ichor.core.files.file import FileContents, ReadFile
+from ichor.core.files.file_data import HasProperties
+from pathlib import Path
+from typing import Dict
+
+
+class ABInt(HasProperties, ReadFile):
+    def __init__(self, path: Union[str, Path]):
+        ReadFile.__init__(self, path)
+        HasProperties.__init__(self)
+
+        self.a: str = FileContents
+        self.b: str = FileContents
+
+        self.iqa_diatomic_contributions: Dict[str, float] = FileContents
+
+        self.total_time: int = FileContents
+
+    @classmethod
+    def check_path(cls, path: Path) -> bool:
+        return path.suffix == cls.filetype and "_" in path.name
+
+    @classproperty
+    def filetype(self) -> str:
+        return ".int"
+
+    @property
+    def properties(self) -> Dict[str, float]:
+        return self.iqa_diatomic_contributions
+
+    @property
+    def e_inter(self):
+        return self.iqa_diatomic_contributions["E_IQA_Inter"]
+
+    def _read_file(self):
+        self.a = ""
+        self.b = ""
+        self.iqa_diatomic_contributions = {}
+        self.total_time = 0
+
+        with open(self.path, "r") as f:
+            line = next(f)
+            while "IQA Atomic and Diatomic Contributions" not in line:
+                line = next(f)
+
+            next(f)  # ---------
+            self.a = next(f).split()[-1]
+            self.b = next(f).split()[-1]
+
+            diatomic_contributions = {}
+            line = next(f)
+            while "=" in line:
+                record = line.split()
+                key = record[0].split("(")[0]
+                value = float(record[1])
+                if key in diatomic_contributions.keys():
+                    diatomic_contributions[key] = (
+                        diatomic_contributions[key] + value
+                    ) / 2
+                else:
+                    diatomic_contributions[key] = value
+                line = next(f)
+
+            self.iqa_diatomic_contributions = diatomic_contributions
+
+            while "Total time" not in line:
+                line = next(f)
+
+            self.total_time = int(line.split()[-2])
