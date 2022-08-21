e9cbddfd88063f069a818a5253b0df6d61ced36a
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-21 20:27:00 +0300

    use namedtuple from collections

--- ichor_core_subpackage/ichor/core/atoms/alf.py
+++ ichor_core_subpackage/ichor/core/atoms/alf.py
@@ -1,12 +1,9 @@
+"""Atomic Local Frame used for one atom. Atomic local frame consists of central atom index,
+x-axis atom index, xy-plane atom index. These indices are 0-indexed. The xy-plane atom
+might not exist if there are only 2 atoms, so default for it is None."""
 
-from typing import NamedTuple, Optional
+from collections import namedtuple
 
-class ALF(NamedTuple):
-    """Atomic Local Frame used for one atom. Atomic local frame consists of central atom index,
-    x-axis atom index, xy-plane atom index. These indices are 0-indexed. The xy-plane atom
-    might not exist if there are only 2 atoms, so default for it is None."""
-
-    origin_idx: int
-    x_axis_idx: int
-    xy_plane_idx: Optional[int] = None
+# default for xy_plane_idx is None as a molecule can have 2 atoms only (e.g. HCl)
+ALF = namedtuple("ALF", "origin_idx x_axis_idx xy_plane_idx", defaults=(None, ))
 
