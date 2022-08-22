10bdc54413434ecb2a53f8c1401653e504bc6132
m-julian <52214154+m-julian@users.noreply.github.com>
2022-08-22 16:30:18 +0300

    import fix

--- ichor_core_subpackage/ichor/core/atoms/list_of_atoms.py
+++ ichor_core_subpackage/ichor/core/atoms/list_of_atoms.py
@@ -195,7 +195,7 @@ class ListOfAtoms(list, ABC):
 
         from ichor.core.atoms import Atom
         from ichor.core.files import Trajectory
-        from ichor.core.useful_functions import alf_features_to_coordinates
+        from ichor.core.calculators import alf_features_to_coordinates
         from ichor.core.common.units import AtomicDistance
 
         if central_atom_name not in self.atom_names:
