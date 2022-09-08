fafde6badd97f2bbfdd1beb39f663f30f38f076b
m-julian <52214154+m-julian@users.noreply.github.com>
2022-09-08 21:12:47 +0300

    look inside the dlpoly job directory for history

--- ichor_core_subpackage/ichor/core/useful_functions/dl_poly/write_final_geometry_to_gjf.py
+++ ichor_core_subpackage/ichor/core/useful_functions/dl_poly/write_final_geometry_to_gjf.py
@@ -1,16 +1,14 @@
 from pathlib import Path
 
-def write_final_geometry_to_gjf(
-    dlpoly_directory: Path,
-) -> Path:
+def write_final_geometry_to_gjf(dlpoly_directory: Path,) -> Path:
 
     from ichor.core.files import GJF, DlpolyHistory
 
-    for d in dlpoly_directory.iterdir():
-        if d.is_dir() and (d / "HISTORY").exists():
-            dlpoly_history = DlpolyHistory(d / "HISTORY")
-            gjf = GJF(d / (d.name + GJF.filetype))
+    for f in dlpoly_directory.iterdir():
+        if f.name == "HISTORY":
+            dlpoly_history = DlpolyHistory(f)
+            gjf = GJF(dlpoly_directory / (f.name + GJF.filetype))
             gjf.atoms = dlpoly_history[-1]
             gjf.write()
             
-    return gjf.path
+    return gjf.path.absolute()
