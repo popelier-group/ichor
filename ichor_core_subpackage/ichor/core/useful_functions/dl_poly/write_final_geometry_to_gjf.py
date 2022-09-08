664d57f947b0aa2a768d0aeaf5f95de181a5fdae
m-julian <52214154+m-julian@users.noreply.github.com>
2022-09-08 21:37:38 +0300

    add kwargs

--- ichor_core_subpackage/ichor/core/useful_functions/dl_poly/write_final_geometry_to_gjf.py
+++ ichor_core_subpackage/ichor/core/useful_functions/dl_poly/write_final_geometry_to_gjf.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
-def write_final_geometry_to_gjf(dlpoly_directory: Path,) -> Path:
+def write_final_geometry_to_gjf(dlpoly_directory: Path, **kwargs) -> Path:
 
     from ichor.core.files import GJF, DlpolyHistory
 
     for f in dlpoly_directory.iterdir():
         if f.name == "HISTORY":
             dlpoly_history = DlpolyHistory(f)
-            gjf = GJF(dlpoly_directory / (f.name + GJF.filetype))
+            gjf = GJF(dlpoly_directory / (f.name + GJF.filetype), **kwargs)
             gjf.atoms = dlpoly_history[-1]
             gjf.write()
             
