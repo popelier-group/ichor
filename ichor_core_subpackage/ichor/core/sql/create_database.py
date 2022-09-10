776bd4a27529609b5001cacea1cdaf0ef0e8370f
m-julian <52214154+m-julian@users.noreply.github.com>
2022-09-10 21:02:39 +0300

    add comments and add all multipole moments

--- ichor_core_subpackage/ichor/core/sql/create_database.py
+++ ichor_core_subpackage/ichor/core/sql/create_database.py
@@ -15,6 +15,8 @@ class AtomNames(Base):
     __tablename__ = "atom_names"
 
     id = Column(Integer, primary_key=True)
+    # these should not be nullable as a PointsDirectory should at least contain geometries
+    # even if no jobs are ran yet
     name = Column(String, unique=True, nullable=False)
     
     children = relationship("Dataset", backref="atom_names")
@@ -26,7 +28,8 @@ class Points(Base):
     id = Column(Integer, primary_key=True)
     date_added = Column(String, nullable=False)
     name = Column(String, nullable=False)
-    wfn_energy = Column(Float, nullable=False)
+    # make nullable because Gaussian might not be ran yet
+    wfn_energy = Column(Float, nullable=True)
     
     children = relationship("Dataset", backref="points")
     
@@ -37,12 +40,20 @@ class Dataset(Base):
     id = Column(Integer, primary_key=True)
     point_id = Column(Integer, ForeignKey("points.id"))
     atom_id = Column(Integer, ForeignKey("atom_names.id"))
+    
+    # coordinates should exist even if jobs are not ran yet in a PointsDirectory
     x = Column(Float, nullable=False)
     y = Column(Float, nullable=False)
     z = Column(Float, nullable=False)
+    
+    # forces might not exist (if Gaussian has not been ran or force keyword has not been used)
+    # so make nullable
     force_x = Column(Float, nullable=True)
     force_y = Column(Float, nullable=True)
     force_z = Column(Float, nullable=True)
+    
+    # AIMAll might not have been ran yet or lower encomp settings used
+    # so make nullable
     iqa_energy = Column(Float, nullable=True)
     integration_error = Column(Float, nullable=True)
     q00 = Column(Float, nullable=True)
@@ -70,8 +81,19 @@ class Dataset(Base):
     q43s = Column(Float, nullable=True)
     q44c = Column(Float, nullable=True)
     q44s = Column(Float, nullable=True)
+    q50 = Column(Float, nullable=True)
+    q51c = Column(Float, nullable=True)
+    q51s = Column(Float, nullable=True)
+    q52c = Column(Float, nullable=True)
+    q52s = Column(Float, nullable=True)
+    q53c = Column(Float, nullable=True)
+    q53s = Column(Float, nullable=True)
+    q54c = Column(Float, nullable=True)
+    q54s = Column(Float, nullable=True)
+    q55c = Column(Float, nullable=True)
+    q55s = Column(Float, nullable=True)
 
-def create_database(database_path: Union[str, Path]):
+def create_database(database_path: Union[str, Path], echo=False):
     """Creates empty database in which important information from a PointsDirectory instance can be stored.
 
     :param database_path: A string or Path to a (non-existing) database on disk.
@@ -80,7 +102,7 @@ def create_database(database_path: Union[str, Path]):
     database_path = str(Path(database_path).absolute())
 
     # create new database and start session
-    engine = create_engine(f"sqlite+pysqlite:///{database_path}", echo=True, future=True)
+    engine = create_engine(f"sqlite+pysqlite:///{database_path}", echo=echo, future=True)
     # Session = sessionmaker(bind=engine)
     # session = Session()
 
