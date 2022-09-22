d9316080002221ed8e1eff5045cef02887f877b8
m-julian <52214154+m-julian@users.noreply.github.com>
2022-09-22 12:12:01 +0100

    fix imports

--- ichor_core_subpackage/ichor/core/molecular_dynamics/cp2k.py
+++ ichor_core_subpackage/ichor/core/molecular_dynamics/cp2k.py
@@ -4,13 +4,9 @@ from typing import Optional
 from ichor.core.useful_functions.get_input import get_files_of_type
 from ichor.core.atoms import Atom, Atoms
 from ichor.core.common.functools import classproperty
-from ichor.core.files import (
-    FileContents,
-    HasAtoms,
-    ReadFile,
-    Trajectory,
-    WriteFile,
-)
+from ichor.core.files import Trajectory
+from ichor.core.files.file import FileContents, ReadFile, WriteFile
+from ichor.core.files.file_data import HasAtoms
 
 
 class CP2KInput(ReadFile, WriteFile, HasAtoms):
