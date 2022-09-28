working, but still gives wrong results

ichor_core_subpackage/ichor/core/models/calculate_fflux_derivatives.py
@@ -8,13 +8,6 @@ import math
 from ichor.core.atoms import ALF
 from ichor.core.models.fflux_derivative_helper_functions import *
 
-def sign_j(fdiff):
-
-    if fdiff <= 0.0:
-        return 1.0
-    else:
-        return -1.0
-
 def fflux_predict_value(model_inst, test_x_features):
 
     x_train_array = model_inst.x
@@ -69,7 +62,7 @@ def fflux_predict_value(model_inst, test_x_features):
 
     return Q_est, dQ_df
 
-def fflux_derivs(iatm_idx, jatm_idx, atoms_instance, model_inst, system_alf):
+def fflux_derivs(iatm_idx, jatm_idx, atoms_instance, system_alf, model_inst):
 
     dQ_dx = 0.0
     dQ_dy = 0.0
@@ -81,31 +74,30 @@ def fflux_derivs(iatm_idx, jatm_idx, atoms_instance, model_inst, system_alf):
     Q_pred, dQ_df = fflux_predict_value(model_inst, jatm_features)
 
     # feature 1
-    df_da = dR_da(atoms_instance, iatm_idx, jatm_idx, 0, system_alf[iatm_idx], jatm_idx)
+    df_da = dR_da(jatm_idx, system_alf[jatm_idx][1], 0, iatm_idx, atoms_instance, system_alf)
     dQ_dx = dQ_dx + dQ_df[0] * df_da[0]
     dQ_dy = dQ_dy + dQ_df[0] * df_da[1]
     dQ_dz = dQ_dz + dQ_df[0] * df_da[2]
     # feature 2
-    df_da = dR_da(atoms_instance, iatm_idx, jatm_idx, 1, system_alf[iatm_idx], jatm_idx)
+    df_da = dR_da(jatm_idx, system_alf[jatm_idx][2], 1, iatm_idx, atoms_instance, system_alf)
     dQ_dx = dQ_dx + dQ_df[1] * df_da[0]
     dQ_dy = dQ_dy + dQ_df[1] * df_da[1]
     dQ_dz = dQ_dz + dQ_df[1] * df_da[2]
     # feature 3
-    df_da = dChi_da(atoms_instance, iatm_idx, system_alf[iatm_idx], jatm_idx)
+    df_da = dChi_da(jatm_idx, iatm_idx, atoms_instance, system_alf)
     dQ_dx = dQ_dx + dQ_df[2] * df_da[0]
     dQ_dy = dQ_dy + dQ_df[2] * df_da[1]
     dQ_dz = dQ_dz + dQ_df[2] * df_da[2]
 
-    # non-local alf atoms
+    # non-alf atoms
     local_non_alf_atoms = [i for i in range(len(atoms_instance)) if i not in system_alf[jatm_idx]]
 
     feat_idx = 3
     for k in range(len(atoms_instance)-3):
-
         diff = atoms_instance[local_non_alf_atoms[k]].coordinates - atoms_instance[jatm_idx].coordinates
 
-        df_da = dR_da(atoms_instance, jatm_idx, local_non_alf_atoms[k], feat_idx, system_alf[jatm_idx], iatm_idx)
-
+        # R
+        df_da = dR_da(jatm_idx, local_non_alf_atoms[k], feat_idx, iatm_idx, atoms_instance, system_alf)
         dQ_dx = dQ_dx + dQ_df[feat_idx] * df_da[0]
         dQ_dy = dQ_dy + dQ_df[feat_idx] * df_da[1]
         dQ_dz = dQ_dz + dQ_df[feat_idx] * df_da[2]
@@ -118,7 +110,7 @@ def fflux_derivs(iatm_idx, jatm_idx, atoms_instance, model_inst, system_alf):
         zeta3 = zetas[2]
 
         # Theta
-        df_da = dTheta_da(atoms_instance, jatm_idx, local_non_alf_atoms[k], system_alf[jatm_idx], feat_idx, zeta3, iatm_idx)
+        df_da = dTheta_da(jatm_idx, local_non_alf_atoms[k], feat_idx, zeta3, iatm_idx, atoms_instance, system_alf)
         dQ_dx = dQ_dx + dQ_df[feat_idx] * df_da[0]
         dQ_dy = dQ_dy + dQ_df[feat_idx] * df_da[1]
         dQ_dz = dQ_dz + dQ_df[feat_idx] * df_da[2]
@@ -126,11 +118,11 @@ def fflux_derivs(iatm_idx, jatm_idx, atoms_instance, model_inst, system_alf):
         feat_idx += 1
 
         # Phi
-        df_da = dPhi_da(atoms_instance, jatm_idx, local_non_alf_atoms[k], system_alf[jatm_idx], zeta1, zeta2, feat_idx, iatm_idx)
+        df_da = dPhi_da(jatm_idx, local_non_alf_atoms[k], zeta1, zeta2, feat_idx, iatm_idx, atoms_instance, system_alf)
         dQ_dx = dQ_dx + dQ_df[feat_idx] * df_da[0]
         dQ_dy = dQ_dy + dQ_df[feat_idx] * df_da[1]
         dQ_dz = dQ_dz + dQ_df[feat_idx] * df_da[2]
 
         feat_idx += 1
 
-    return dQ_dx, dQ_dy, dQ_dz
+    return np.array([dQ_dx, dQ_dy, dQ_dz])

ichor_core_subpackage/ichor/core/models/fflux_derivative_helper_functions.py
@@ -1,6 +1,12 @@
 import numpy as np
 from ichor.core.calculators import calculate_alf_features
 
+def sign_j(fdiff):
+    if fdiff <= 0.0:
+        return 1.0
+    else:
+        return -1.0
+
 def sigma_fflux(x_axis_diff, xy_plane_diff):
     return -np.dot(x_axis_diff, xy_plane_diff) / np.dot(x_axis_diff, x_axis_diff)
 
@@ -44,8 +50,7 @@ def delta_An(iatm_idx, jatm_idx, omega):
         return 0.0
 
 def C_matrix(atoms_instance, iatm_idx, system_alf):
-    iatm_alf = system_alf[iatm_idx]
-    return atoms_instance[iatm_idx].C(iatm_alf)
+    return atoms_instance[iatm_idx].C(system_alf)
 
 def C_1(iatm_idx, atoms_instance, system_alf):
     return C_matrix(atoms_instance, iatm_idx, system_alf)[0]
@@ -55,7 +60,7 @@ def C_2(iatm_idx, atoms_instance, system_alf):
 
 def C_3(iatm_idx, atoms_instance, system_alf):
     return C_matrix(atoms_instance, iatm_idx, system_alf)[2]
-
+ 
 def dRaxdotRaxy_da(iatm_idx, omega, diff1, diff2, system_alf):
     iatm_alf = system_alf[iatm_idx]
     if omega == iatm_idx:
@@ -80,12 +85,12 @@ def dRaxdotRax_da(iatm_idx, omega, diff, system_alf):
 
 def dsigma_da(iatm_idx, omega, atoms_instance, system_alf):
     iatm_alf = system_alf[iatm_idx]
-    diff = atoms_instance[iatm_alf[1]].coordinates - atoms_instance[iatm_idx]
-    diff2 = atoms_instance[iatm_alf[2]].coordinates - atoms_instance[iatm_idx]
+    diff = atoms_instance[iatm_alf[1]].coordinates - atoms_instance[iatm_idx].coordinates
+    diff2 = atoms_instance[iatm_alf[2]].coordinates - atoms_instance[iatm_idx].coordinates
     RaxdotRax = np.dot(diff, diff)
     RaxdotRaxy = np.dot(diff, diff2)
-    res = ((RaxdotRaxy * dRaxdotRax_da(iatm_idx, iatm_alf, omega, diff)) / (RaxdotRax**2)) \
-                - (dRaxdotRaxy_da(iatm_idx, iatm_alf, omega, diff, diff2))
+    res = ((RaxdotRaxy * dRaxdotRax_da(iatm_idx, omega, diff, system_alf)) / (RaxdotRax**2)) \
+                - (dRaxdotRaxy_da(iatm_idx, omega, diff, diff2, system_alf) / RaxdotRax)
 
     return res
 
@@ -103,21 +108,20 @@ def dyj_da(iatm_idx, omega, diff, atoms_instance, system_alf):
     s_fflux = sigma_fflux(x_axis_diff, xy_plane_diff)
     for i in range(3):
         for k in range(3):
-            res[i, k] = (dsigma[i] * diff[k]) + (s_fflux * kronecker(i,k)*delta_OmAx) + (kronecker(i,k)*delta_OmAxy)
+            res[i, k] = (dsigma[i] * diff[k]) + (s_fflux * kronecker(i,k) * delta_OmAx) + (kronecker(i,k) * delta_OmAxy)
     return res
 
 def EqC12(iatm_idx, omega, diff, atoms_instance, system_alf):
-    eqC12 = np.zeros(3)
-    iatm_alf = system_alf[iatm_idx]
+    res = np.zeros(3)
     dyj = dyj_da(iatm_idx, omega, diff, atoms_instance, system_alf)
     y_v = y_vec(iatm_idx, atoms_instance, system_alf)
     for j in range(3):
-        eqC12[0] = eqC12[0] + (y_v[j] * dyj[0, j])
-        eqC12[1] = eqC12[1] + (y_v[j] * dyj[1, j])
-        eqC12[2] = eqC12[2] + (y_v[j] * dyj[2, j])
+        res[0] = res[0] + (y_v[j] * dyj[0, j])
+        res[1] = res[1] + (y_v[j] * dyj[1, j])
+        res[2] = res[2] + (y_v[j] * dyj[2, j])
     sqrtydoty = np.sqrt(np.dot(y_v, y_v))
     tmp = -1.0 / sqrtydoty**3
-    return eqC12 * tmp
+    return res * tmp
 
 def dR_da(iatm_idx, jatm_idx, feat_idx, omega, atoms_instance, system_alf):
     iatm_alf = system_alf[iatm_idx]
@@ -141,7 +145,7 @@ def dC1k_da(iatm_idx, omega, atoms_instance, system_alf):
     delta_OmAx = delta_Ax(iatm_idx, omega, system_alf)
     for i in range(3):
         for k in range(3):
-            res[i, k] = (invRAx * delta_OmAx * kronecker(i,k)) - (diff(i) * diff(k) * delta_OmAx * inv_RAx3)
+            res[i, k] = (invRAx * delta_OmAx * kronecker(i,k)) - (diff[i] * diff[k] * delta_OmAx * inv_RAx3)
     return res
 
 def dC2k_da(iatm_idx, omega, atoms_instance, system_alf):
@@ -151,7 +155,7 @@ def dC2k_da(iatm_idx, omega, atoms_instance, system_alf):
     invsqrt_ydoty = 1.0 / np.sqrt(np.dot(y_v, y_v))
     diff = atoms_instance[iatm_alf[1]].coordinates - atoms_instance[iatm_idx].coordinates
     eqC12 = EqC12(iatm_idx, omega, diff, atoms_instance, system_alf)
-    dyj = dyj_da(atoms_instance, iatm_idx, iatm_alf, omega, diff)
+    dyj = dyj_da(iatm_idx, omega, diff, atoms_instance, system_alf)
     for k in range(3):
         res[0, k] = (dyj[0,k] * invsqrt_ydoty) + (y_v[k] * eqC12[0])
         res[1, k] = (dyj[1,k] * invsqrt_ydoty) + (y_v[k] * eqC12[1])
@@ -172,14 +176,12 @@ def dC3k_da(c1_vec, c2_vec, dc1k, dc2k):
     return res
 
 def dZj_da(iatm_idx, jatm_idx, omega, j, atoms_instance, system_alf):
-    iatm_alf = system_alf[iatm_idx]
     diff = atoms_instance[jatm_idx].coordinates - atoms_instance[iatm_idx].coordinates
-    dzj_da = np.zeros(3)
-    zeta = np.zeros(3)
-    c = C_matrix(atoms_instance, iatm_idx, iatm_alf)
-    C1_vec = c[0]
-    C2_vec = c[1]
-    C3_vec = c[2]
+    res = np.zeros(3)
+    # zeta = np.zeros(3)
+    C1_vec = C_1(iatm_idx, atoms_instance, system_alf)
+    C2_vec = C_2(iatm_idx, atoms_instance, system_alf)
+    C3_vec = C_3(iatm_idx, atoms_instance, system_alf)
     if j == 1:
         Cjk = C1_vec
         dCjk = dC1k_da(iatm_idx, omega, atoms_instance, system_alf)
@@ -190,11 +192,11 @@ def dZj_da(iatm_idx, jatm_idx, omega, j, atoms_instance, system_alf):
         Cjk = C3_vec
         dC1k = dC1k_da(iatm_idx, omega, atoms_instance, system_alf)
         dC2k = dC2k_da(iatm_idx, omega, atoms_instance, system_alf)
-        dCjk = dC3k_da(iatm_idx, iatm_alf, omega, C1_vec, C2_vec, dC1k, dC2k)
+        dCjk = dC3k_da(C1_vec, C2_vec, dC1k, dC2k)
 
     for i in range(3):
         for k in range(3):
-            dzj_da[i] = dzj_da[i] + dCjk[i,k] * diff[k] + Cjk[k] * kronecker(i,k) * delta_An(iatm_idx, jatm_idx, omega)
+            res[i] = res[i] + dCjk[i,k] * diff[k] + Cjk[k] * kronecker(i,k) * delta_An(iatm_idx, jatm_idx, omega)
 
     # zeta[0] = C1_vec[0] * diff[0] + C1_vec[1] * diff[1] + C1_vec[2] * diff[2]
     # zeta[1] = C2_vec[0] * diff[0] + C2_vec[1] * diff[1] + C2_vec[2] * diff[2]
@@ -202,7 +204,7 @@ def dZj_da(iatm_idx, jatm_idx, omega, j, atoms_instance, system_alf):
 
     # return dzj_da, zeta
 
-    return dzj_da
+    return res
 
 def dChi_da(iatm_idx, omega, atoms_instance, system_alf):
     iatm_alf = system_alf[iatm_idx]
@@ -215,41 +217,32 @@ def dChi_da(iatm_idx, omega, atoms_instance, system_alf):
     invsin_Chi = -1.0 / np.sin(iatm_features[2])
     res = ( ( -1.0 * dR_da(iatm_idx, system_alf[iatm_idx][1], 0, omega, atoms_instance, system_alf) * RxdotRxy ) / (R_Ax**2 * R_Axy) )\
             + ( dRaxdotRaxy_da(iatm_idx, omega, diff1, diff2, system_alf) / (R_Ax * R_Axy) ) \
-            + ( -1.0 * dR_da(iatm_idx, system_alf[iatm_idx][2], 1, omega, atoms_instance, system_alf) * RxdotRxy / (R_Ax * R_Axy**2))
+            + ( -1.0 * dR_da(iatm_idx, system_alf[iatm_idx][2], 1, omega, atoms_instance, system_alf) * RxdotRxy / (R_Ax * R_Axy**2) )
     return res * invsin_Chi
 
-def dTheta_da(atoms_instance, iatm_idx, jatm_idx, iatm_alf, theta_feat_idx, zeta3, omega):
-
-    dTheta = np.zeros(3)
-
+def dTheta_da(iatm_idx, jatm_idx, theta_feat_idx, zeta3, omega, atoms_instance, system_alf):
+    iatm_alf = system_alf[iatm_idx]
+    res = np.zeros(3)
     iatm_features = atoms_instance[iatm_idx].features(calculate_alf_features, iatm_alf)
     R_An = iatm_features[theta_feat_idx-1]
     Theta = iatm_features[theta_feat_idx]
     inv_RAn = 1.0 / R_An
     invSinTheta = -1.0 / np.sin(Theta)
-    dInvR = -1.0 * inv_RAn**2 * dR_da(atoms_instance, iatm_idx, jatm_idx, theta_feat_idx-1, iatm_alf, omega)
+    dInvR = -1.0 * inv_RAn**2 * dR_da(iatm_idx, jatm_idx, theta_feat_idx-1, omega, atoms_instance, system_alf)
+    dz3 = dZj_da(iatm_idx, jatm_idx, omega, 3, atoms_instance, system_alf)
+    res[0] = invSinTheta * (zeta3 * dInvR[0] + inv_RAn * dz3[0])
+    res[1] = invSinTheta * (zeta3 * dInvR[1] + inv_RAn * dz3[1])
+    res[2] = invSinTheta * (zeta3 * dInvR[2] + inv_RAn * dz3[2])
+    return res
 
-    dzj = dZj_da(atoms_instance, iatm_idx, jatm_idx, iatm_alf, omega, 3)
-
-    dTheta[0] = invSinTheta * (zeta3 * dInvR[0] + inv_RAn * dzj[0])
-    dTheta[1] = invSinTheta * (zeta3 * dInvR[1] + inv_RAn * dzj[1])
-    dTheta[2] = invSinTheta * (zeta3 * dInvR[2] + inv_RAn * dzj[2])
-
-    return dTheta
-
-def dPhi_da(atoms_instance, iatm_idx, jatm_idx, iatm_alf, zeta1, zeta2, phi_feat_idx, omega):
-
-    dPhi = np.zeros(3)
-
+def dPhi_da(iatm_idx, jatm_idx, zeta1, zeta2, phi_feat_idx, omega, atoms_instance, system_alf):
+    iatm_alf = system_alf[iatm_idx]
+    res = np.zeros(3)
     iatm_features = atoms_instance[iatm_idx].features(calculate_alf_features, iatm_alf)
-
-    cos2phi = np.cos(iatm_features[phi_feat_idx])
-
-    dz1 = dZj_da(atoms_instance, iatm_idx, jatm_idx, iatm_alf, omega, 1)
-    dz2 = dZj_da(atoms_instance, iatm_idx, jatm_idx, iatm_alf, omega, 2)
-
-    dPhi[0] = cos2phi * ( ((-1.0 * zeta2 * dz1[0]) / (zeta1**2)) + dz2[0]/zeta1)
-    dPhi[1] = cos2phi * ( ((-1.0 * zeta2 * dz1[1]) / (zeta1**2)) + dz2[1]/zeta1)
-    dPhi[2] = cos2phi * ( ((-1.0 * zeta2 * dz1[2]) / (zeta1**2)) + dz2[2]/zeta1)
-
-    return dPhi
+    cos2phi = np.power(np.cos(iatm_features[phi_feat_idx]), 2)
+    dz1 = dZj_da(iatm_idx, jatm_idx, omega, 1, atoms_instance, system_alf)
+    dz2 = dZj_da(iatm_idx, jatm_idx, omega, 2, atoms_instance, system_alf)
+    res[0] = cos2phi * ( ((-1.0 * zeta2 * dz1[0]) / (zeta1**2)) + (dz2[0]/zeta1) )
+    res[1] = cos2phi * ( ((-1.0 * zeta2 * dz1[1]) / (zeta1**2)) + (dz2[1]/zeta1) )
+    res[2] = cos2phi * ( ((-1.0 * zeta2 * dz1[2]) / (zeta1**2)) + (dz2[2]/zeta1) )
+    return res