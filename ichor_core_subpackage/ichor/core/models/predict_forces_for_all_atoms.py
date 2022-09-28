3ec5b74541fd8b5c0dccb6c7573bbe461d8e2100
m-julian <52214154+m-julian@users.noreply.github.com>
2022-09-28 19:36:00 +0100

    add typing, docstrings and dict method

--- ichor_core_subpackage/ichor/core/models/predict_forces_for_all_atoms.py
+++ ichor_core_subpackage/ichor/core/models/predict_forces_for_all_atoms.py
@@ -1,11 +1,24 @@
-from ichor.core.files import XYZ
-from ichor.core.models.model import Model
 import numpy as np
 from ichor.core.models.calculate_fflux_derivatives import fflux_derivs
 from ichor.core.atoms import ALF
-from typing import List
+from typing import List, Dict
 
-def predict_fflux_forces_for_all_atoms(atoms: "Atoms", models: "Models", system_alf: List["ALF"]):
+def predict_fflux_forces_for_all_atoms(atoms: "Atoms", models: "Models", system_alf: List["ALF"]) -> np.ndarray:
+    """Predicts the forces that FFLUX predicts (which are written to IQA_FORCES file).
+
+    .. note::
+        The atoms instance is converted to Bohr internally because the forces are per Bohr in FFLUX.
+        Also the `system_alf` argument is 0-indexed (as calcualted by ichor methods),
+        while the ALF in the .model files is 1-indexed.
+
+    :param atoms: An Atoms instance containing the geometry for which to predict forces.
+        Note that the ordering of the atoms matters, i.e. the index of the atoms in the Atoms instance
+        must match the index of the model files.
+    :param models: A models instance which wraps around a directory containing model files.
+    :param system_alf: The system alf as a list of `ALF` instances (the `ALF` instances are just a named tuple
+        containing origin_idx, x-axis_idx, xy_plane_idx. It is 0-indexed.)
+    :return: A np.ndarray of shape n_atoms x 3 containing the x,y,z force for every atom
+    """
 
     atomic_forces_list = []
 
@@ -36,4 +49,24 @@ def predict_fflux_forces_for_all_atoms(atoms: "Atoms", models: "Models", system_
         atomic_forces_list.append(local_forces)
 
     return np.array(atomic_forces_list)
-    
+
+def predict_fflux_forces_for_all_atoms_dict(atoms: "Atoms", models: "Models", system_alf: List["ALF"]) -> Dict[str, np.ndarray]:
+    """Predicts the forces that FFLUX predicts (which are written to IQA_FORCES file).
+
+    .. note::
+        The atoms instance is converted to Bohr internally because the forces are per Bohr in FFLUX.
+        Also the `system_alf` argument is 0-indexed (as calcualted by ichor methods),
+        while the ALF in the .model files is 1-indexed.
+
+    :param atoms: An Atoms instance containing the geometry for which to predict forces.
+        Note that the ordering of the atoms matters, i.e. the index of the atoms in the Atoms instance
+        must match the index of the model files.
+    :param models: A models instance which wraps around a directory containing model files.
+    :param system_alf: The system alf as a list of `ALF` instances (the `ALF` instances are just a named tuple
+        containing origin_idx, x-axis_idx, xy_plane_idx. It is 0-indexed.)
+    :return: A dictionary of key: atom_name, value: 1D np.ndarray containing the x,y,z forces on the atom.
+    """
+    atom_names = atoms.atom_names
+    forces = predict_fflux_forces_for_all_atoms(atoms, models, system_alf)
+
+    return dict(zip(atom_names, forces))
