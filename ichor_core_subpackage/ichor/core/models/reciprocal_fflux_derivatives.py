5ac104fb79e5916d58ecd7363dfe3212d0bda47f
m-julian <52214154+m-julian@users.noreply.github.com>
2022-11-14 13:39:12 +0000

    add comments

--- ichor_core_subpackage/ichor/core/models/reciprocal_fflux_derivatives.py
+++ ichor_core_subpackage/ichor/core/models/reciprocal_fflux_derivatives.py
@@ -29,23 +29,21 @@ def reciprocal_fflux_derivs_da_df(iatm_idx, jatm_idx, atoms_instance, system_alf
     # feature 1, this is a np.ndarray of shape 3, derivative of global coordinate x with respect to first feature,
     # derivative of global coordinate y with r espect to first feature, derivative of global coord z wtr first feature.
 
-    # da1 / df1, da2 / df1, da3 / df1
+    # derivative of one atom (A) w.r.t. feature 1 of atom omega (the atom on which the ALF is centered) da^A_1 / df1, da^A_2 / df1, da^A_3 / df1 
     df_da = dR_da(jatm_idx, system_alf[jatm_idx][1], 0, iatm_idx, atoms_instance, system_alf)
     da_df = np.divide(1.0, df_da, out=np.zeros_like(df_da), where= np.abs(df_da) > np.finfo(float).eps)
     n_features_times_3_tmp_matrix[0, 0] = da_df[0]
     n_features_times_3_tmp_matrix[0, 1] = da_df[1]
     n_features_times_3_tmp_matrix[0, 2] = da_df[2]
 
-    # feature 2
-    # da1 / df2, da2/ df2, da3 / df2
+    # derivative of one atom (A) w.r.t. feature 2 of atom omega (the atom on which the ALF is centered) da^A_1 / df2, da^A_2 / df2, da^A_3 / df2 
     df_da = dR_da(jatm_idx, system_alf[jatm_idx][2], 1, iatm_idx, atoms_instance, system_alf)
     da_df = np.divide(1.0, df_da, out=np.zeros_like(df_da), where= np.abs(df_da) > np.finfo(float).eps)
     n_features_times_3_tmp_matrix[1, 0] = da_df[0]
     n_features_times_3_tmp_matrix[1, 1] = da_df[1]
     n_features_times_3_tmp_matrix[1, 2] = da_df[2]
 
-    # feature 3
-    # da1 / df3, da2/ df3, da3 / df3
+    # derivative of one atom (A) w.r.t. feature 3 of atom omega (the atom on which the ALF is centered) da^A_1 / df3, da^A_2 / df3, da^A_3 / df3 
     df_da = dChi_da(jatm_idx, iatm_idx, atoms_instance, system_alf)
     da_df = np.divide(1.0, df_da, out=np.zeros_like(df_da), where= np.abs(df_da) > np.finfo(float).eps)
     n_features_times_3_tmp_matrix[2, 0] = da_df[0]
@@ -55,6 +53,7 @@ def reciprocal_fflux_derivs_da_df(iatm_idx, jatm_idx, atoms_instance, system_alf
     # non-alf atoms
     local_non_alf_atoms = [i for i in range(len(atoms_instance)) if i not in system_alf[jatm_idx]]
 
+    # derivative of one atom (A) w.r.t. feature k of atom omega (the atom on which the ALF is centered) da^A_1 / df_k, da^A_2 / df_k, da^A_3 / df_k 
     feat_idx = 3
     for k in range(len(atoms_instance)-3):
         diff = atoms_instance[local_non_alf_atoms[k]].coordinates - atoms_instance[jatm_idx].coordinates
