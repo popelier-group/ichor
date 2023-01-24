996890ae50ab2c3244dc5013020f51d97b3de184
m-julian <52214154+m-julian@users.noreply.github.com>
2023-01-24 21:30:13 +0000

    bug fix with variable name

--- ichor_core_subpackage/ichor/core/sql/query_database.py
+++ ichor_core_subpackage/ichor/core/sql/query_database.py
@@ -205,7 +205,7 @@ def write_processed_one_atom_data_to_csv(full_df: pd.DataFrame, point_ids: List[
             n_features = len(one_atom_features)
 
             # default values to be written if forces do not exist
-            global_forces_array = None, None, None
+            local_forces_array = None, None, None
             # if forces are also in database then calculate local forces
             if row_with_atom_info["force_x"].item() is not None:
 
