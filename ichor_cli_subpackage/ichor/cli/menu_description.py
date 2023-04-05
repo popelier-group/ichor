349f7c5b8edadce304945155ab4ae0d0f4789c58
m-julian <52214154+m-julian@users.noreply.github.com>
2023-04-05 18:44:16 +0100

    default exit option set to True

--- ichor_cli_subpackage/ichor/cli/menu_description.py
+++ ichor_cli_subpackage/ichor/cli/menu_description.py
@@ -7,4 +7,6 @@ class MenuDescription:
     subtitle: str = ""
     prologue_description_text: str = ""
     epilogue_description_text: str = ""
-    show_exit_option: bool = False
+    # note that the exit option becomes f"Return to {parent_menu}" for child menus.
+    # Setting this to false means you cannot get back to parent menus from child menus.
+    show_exit_option: bool = True
