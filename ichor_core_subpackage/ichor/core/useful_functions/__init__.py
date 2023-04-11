5db80c0664fcd530db8457b161cfce212672fa2d
m-julian <52214154+m-julian@users.noreply.github.com>
2023-04-11 15:41:43 +0100

    bug fix

--- ichor_core_subpackage/ichor/core/useful_functions/__init__.py
+++ ichor_core_subpackage/ichor/core/useful_functions/__init__.py
@@ -3,10 +3,6 @@ from ichor.core.useful_functions.get_atoms import (
     get_atoms_from_path,
     get_trajectory_from_path,
 )
-from ichor.core.useful_functions.get_models import (
-    get_models_from_path,
-    number_of_models_in_dir,
-)
 from ichor.core.useful_functions.get_path import (
     get_dir,
     get_file,
