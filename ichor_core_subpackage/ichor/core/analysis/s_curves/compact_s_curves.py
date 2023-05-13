99ec51d88f2e9e6df015cc2be7a132bdd06ee3f8
m-julian <52214154+m-julian@users.noreply.github.com>
2023-05-13 16:26:49 +0100

    improve fontsizes

--- ichor_core_subpackage/ichor/core/analysis/s_curves/compact_s_curves.py
+++ ichor_core_subpackage/ichor/core/analysis/s_curves/compact_s_curves.py
@@ -470,26 +470,29 @@ def plot_with_matplotlib(
                 array_sorted = np.sort(np.absolute(array))
                 perc = percentile(array_sorted.shape[0])
 
-                ax.plot(array_sorted, perc, label=an)
+                ax.plot(array_sorted, perc, label=an, linewidth=2)
                 ax.set_xscale("log")
 
-    plt.legend(facecolor="white", framealpha=1, frameon=True)
+    plt.legend(facecolor="white", framealpha=1, frameon=True, fontsize=24)
 
     # Show the major grid and style it slightly.
-    ax.grid(which="major", color="#DDDDDD", linewidth=0.8)
+    ax.grid(which="major", color="#DDDDDD", linewidth=1.2)
     # Show the minor grid as well. Style it in very light gray as a thin,
     # dotted line.
-    ax.grid(which="minor", color="#EEEEEE", linestyle=":", linewidth=0.8)
+    ax.grid(which="minor", color="#EEEEEE", linestyle=":", linewidth=1.0)
     # Make the minor ticks and gridlines show.
     ax.minorticks_on()
     ax.grid(True)
 
     if x_axis_name:
-        ax.set_xlabel(x_axis_name, fontsize=14)
+        ax.set_xlabel(x_axis_name, fontsize=24)
     if y_axis_name:
-        ax.set_ylabel(y_axis_name, fontsize=14)
+        ax.set_ylabel(y_axis_name, fontsize=24)
     if title:
-        ax.set_title(title, fontsize=16)
+        ax.set_title(title, fontsize=28)
+
+    ax.tick_params(axis="both", which="major", labelsize=18)
+    ax.tick_params(axis="both", which="minor", labelsize=18)
 
     plt.show()
 
