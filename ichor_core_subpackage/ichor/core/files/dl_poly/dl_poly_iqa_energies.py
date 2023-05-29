f816d3ce9886afe39eb4a206d21a73e7fb90f8c5
m-julian <52214154+m-julian@users.noreply.github.com>
2023-05-29 14:25:36 +0100

    imporve file reading

--- ichor_core_subpackage/ichor/core/files/dl_poly/dl_poly_iqa_energies.py
+++ ichor_core_subpackage/ichor/core/files/dl_poly/dl_poly_iqa_energies.py
@@ -10,14 +10,19 @@ class DlPolyIQAEnergies(ReadFile):
     """READS the IQA_ENERGIES file from FFLUX.
 
     :param path: Path to IQA_ENERGIES file
-    :param natoms: Number of atoms in the system.
+
+    :ivar natoms: Number of atoms in system
+    :ivar energies: Array of shape ntimesteps x natoms for read energies
     """
 
-    def __init__(self, path: Union[Path, str], natoms: int):
+    def __init__(self, path: Union[Path, str]):
 
         super().__init__(path)
-        self.natoms = natoms
         self.energies = FileContents
+
+    @classmethod
+    def check_path(cls, path: Path) -> bool:
+        return path.stem == "IQA_ENERGIES"
 
     def _read_file(self):
 
@@ -25,14 +30,22 @@ class DlPolyIQAEnergies(ReadFile):
 
         with open(self.path, "r") as f:
 
+            lines = [i.strip().split() for i in f.readlines()]
+            # lambda func that maps first argument as int and others as float
+            map_func = lambda x: [int(x[0]), *map(float, x[1:])]
+            # list of lists. Each inner list has index, energy
+            lines = list(map(map_func, lines))
+
+            max_atom_idx = max(i[0] for i in lines)
+            self.natoms = max_atom_idx
+
             one_timestep_energies = []
             counter = 0
-            for line in f:
+            for line in lines:
                 # note that atom index is 1-indexed
                 # also if using OpenMP, these are not going to be ordered correctly
                 # so need to reorder them every timestep
-                atom_index, iqa_energy = line.strip().split()
-                atom_index, iqa_energy = int(atom_index), float(iqa_energy)
+                atom_index, iqa_energy = line
                 one_timestep_energies.append((atom_index, iqa_energy))
                 counter += 1
 
@@ -45,5 +58,5 @@ class DlPolyIQAEnergies(ReadFile):
                     one_timestep_energies = []
                     counter = 0
 
-            # save as matrix of natoms x ntimesteps
+            # save as matrix of ntimesteps x natoms
             self.energies = np.array(energies)
--- ichor_core_subpackage/ichor/core/files/dl_poly/dl_poly_iqa_forces.py
+++ ichor_core_subpackage/ichor/core/files/dl_poly/dl_poly_iqa_forces.py
@@ -10,17 +10,21 @@ class DlPolyIQAForces(ReadFile):
     """READS the IQA_FORCES file from FFLUX.
 
     :param path: Path to IQA_FORCES file
-    :param natoms: Number of atoms in the system.
 
     :ivar forces: The forces array of shape ntimesteps x natoms x 3.
         Initialized as FileContents prior to file reading.
+    :ivar natoms: Number of atoms in each timestep
     """
 
-    def __init__(self, path: Union[Path, str], natoms: int):
+    def __init__(self, path: Union[Path, str]):
 
         super().__init__(path)
-        self.natoms = natoms
         self.forces = FileContents
+        self.natoms = FileContents
+
+    @classmethod
+    def check_path(cls, path: Path) -> bool:
+        return path.stem == "IQA_FORCES"
 
     def _read_file(self):
 
@@ -28,15 +32,22 @@ class DlPolyIQAForces(ReadFile):
 
         with open(self.path, "r") as f:
 
+            lines = [i.strip().split() for i in f.readlines()]
+            # lambda func that maps first argument as int and others as float
+            map_func = lambda x: [int(x[0]), *map(float, x[1:])]
+            # list of lists. Each inner list has index, cartesian forces
+            lines = list(map(map_func, lines))
+            max_atom_idx = max(i[0] for i in lines)
+            self.natoms = max_atom_idx
+
             one_timestep_forces = []
             counter = 0
 
-            for line in f:
+            for line in lines:
 
                 # note that atom index is 1-indexed
                 # seems like the ordering of IQA_FORCES is not affected by OpenMP
-                cartesian_forces = line.strip().split()[1:]
-                cartesian_forces = list(map(float, cartesian_forces))
+                cartesian_forces = line[1:]
                 one_timestep_forces.append(cartesian_forces)
                 counter += 1
 
