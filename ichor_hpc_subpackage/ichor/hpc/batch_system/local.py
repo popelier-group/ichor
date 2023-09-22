5ad2b4d2ad515825332918c7c1c7cdc57002d906
m-julian <52214154+m-julian@users.noreply.github.com>
2023-09-22 17:28:33 +0100

    add local batch system methods

--- ichor_hpc_subpackage/ichor/hpc/batch_system/local.py
+++ ichor_hpc_subpackage/ichor/hpc/batch_system/local.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from ichor.core.common.functools import classproperty
 from ichor.hpc.batch_system.node import NodeType
@@ -27,3 +27,35 @@ class LocalBatchSystem:
     @classmethod
     def parse_job_id(cls, stdout) -> List[str]:
         return ["test1234"]
+
+    @classmethod
+    def change_working_directory(cls, dir):
+        return "."
+
+    @classmethod
+    def output_directory(cls, d, task_array=False):
+        return "."
+
+    @classmethod
+    def error_directory(cls, d, task_array=False):
+        return "."
+
+    @classmethod
+    def parallel_environment(cls, cores):
+        return 2
+
+    @property
+    def OptionCmd(self) -> str:
+        return "test"
+
+    @classmethod
+    def array_job(cls, njobs: int, max_running_tasks: Optional[int] = None) -> str:
+        """Returns the line in the job script that specifies this job is an array job.
+        These jobs are run at the same time in parallel
+        as they do not depend on one another. An example will be running 50 Gaussian
+        or AIMALL jobs at the same time without having to submit
+        50 separate jobs. Instead 1 array job can be submitted."""
+        array_str = f"-a 1-{njobs}"
+        if max_running_tasks is not None:
+            array_str += f"{min(njobs-1, max_running_tasks)}"
+        return array_str
