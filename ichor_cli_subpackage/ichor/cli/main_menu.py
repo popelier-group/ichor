41c4d11e246096fae2c1b19ebbe509f39dcdff6a
m-julian <52214154+m-julian@users.noreply.github.com>
2023-12-26 01:10:55 +0200

    add submenus and move scripts to hpc

--- ichor_cli_subpackage/ichor/cli/main_menu.py
+++ ichor_cli_subpackage/ichor/cli/main_menu.py
@@ -6,6 +6,10 @@ from ichor.cli.main_menu_submenus.analysis_menu import (
     analysis_menu,
     ANALYSIS_MENU_DESCRIPTION,
 )
+from ichor.cli.main_menu_submenus.database_menu.submit_csvs_from_database import (
+    submit_csvs_menu,
+    SUBMIT_CSVS_MENU_DESCRIPTION,
+)
 from ichor.cli.main_menu_submenus.molecular_dynamics_menu.molecular_dynamics_menu import (
     molecular_dynamics_menu,
     MOLECULAR_DYNAMICS_MENU_DESCRIPTION,
@@ -22,6 +26,7 @@ from ichor.cli.main_menu_submenus.trajectory_menu.trajectory_menu import (
     trajectory_menu,
     TRAJECTORY_MENU_DESCRIPTION,
 )
+
 from ichor.cli.menu_description import MenuDescription
 from ichor.cli.menu_options import MenuOptions
 
@@ -56,6 +61,9 @@ main_menu = ConsoleMenu(
 main_menu_items = [
     SubmenuItem(
         POINTS_DIRECTORY_MENU_DESCRIPTION.title, points_directory_menu, main_menu
+    ),
+    SubmenuItem(
+        SUBMIT_CSVS_MENU_DESCRIPTION.title, submit_csvs_menu, points_directory_menu
     ),
     SubmenuItem(TRAJECTORY_MENU_DESCRIPTION.title, trajectory_menu, main_menu),
     SubmenuItem(ANALYSIS_MENU_DESCRIPTION.title, analysis_menu, main_menu),
--- ichor_cli_subpackage/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_menu.py
+++ ichor_cli_subpackage/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_menu.py
@@ -1,4 +1,3 @@
-import math
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Union
@@ -17,21 +16,7 @@ from ichor.cli.main_menu_submenus.points_directory_menu.points_directory_submenu
 )
 from ichor.cli.menu_description import MenuDescription
 from ichor.cli.menu_options import MenuOptions
-from ichor.cli.useful_functions import (
-    bool_to_str,
-    compile_strings_to_python_code,
-    user_input_bool,
-    user_input_float,
-    user_input_int,
-    user_input_path,
-)
-from ichor.core.sql.query_database import (
-    get_alf_from_first_db_geometry,
-    write_processed_data_for_atoms_parallel,
-)
-from ichor.hpc.global_variables import SCRIPT_NAMES
-from ichor.hpc.submission_commands.free_flow_python_command import FreeFlowPythonCommand
-from ichor.hpc.submission_script import SubmissionScript
+from ichor.cli.useful_functions import user_input_path
 
 POINTS_DIRECTORY_MENU_DESCRIPTION = MenuDescription(
     "PointsDirectory Menu",
@@ -64,128 +49,14 @@ class PointsDirectoryFunctions:
 
     @staticmethod
     def select_points_directory():
-        """Asks user to update points directory and then updates PointsDirectoryMenuPrologue instance."""
-        pd_path = user_input_path("Enter PointsDirectory Path: ")
+        """Asks user to update points directory and then updates PointsDirectoryMenuOptions instance."""
+        pd_path = user_input_path("Change PointsDirectory Path: ")
         ichor.cli.global_menu_variables.SELECTED_POINTS_DIRECTORY_PATH = Path(
             pd_path
         ).absolute()
         points_directory_menu_options.selected_points_directory_path = (
             ichor.cli.global_menu_variables.SELECTED_POINTS_DIRECTORY_PATH
         )
-
-    @staticmethod
-    def make_csvs_from_database():
-        """Makes csv files contaning features, iqa energies, and rotated multipole moments given a database"""
-
-        default_submit_on_compute = True
-        default_rotate_multipoles = True
-        default_calculate_feature_forces = False
-        default_filter_by_sum_iqa_compared_to_wfn = True
-        default_float_difference_iqa_wfn = 4.184
-        default_filter_by_integration_error = True
-        default_float_integration_error = 0.001
-        default_ncores = 4
-
-        db_path = user_input_path("Enter path to SQLite3 database: ")
-        db_path = Path(db_path)
-
-        rotate_multipoles = user_input_bool(
-            f"Calculate rotated multipoles (yes/no), default {bool_to_str(default_rotate_multipoles)}: "
-        )
-        if rotate_multipoles is None:
-            rotate_multipoles = default_rotate_multipoles
-
-        calculate_feature_forces = user_input_bool(
-            f"Calculate feature forces (yes/no), default {bool_to_str(default_calculate_feature_forces)}: "
-        )
-        if calculate_feature_forces is None:
-            calculate_feature_forces = default_calculate_feature_forces
-
-        # filtering by sum of iqa vs wfn energy
-        filter_by_sum_iqa_vs_wfn = user_input_bool(
-            f"Filter by comparing sum of IQA to WFN (yes/no), default {bool_to_str(default_filter_by_sum_iqa_compared_to_wfn)}: "  # noqa: E501
-        )
-        if filter_by_sum_iqa_vs_wfn is None:
-            filter_by_sum_iqa_vs_wfn = default_filter_by_sum_iqa_compared_to_wfn
-
-        if filter_by_sum_iqa_vs_wfn:
-            float_difference_iqa_wfn = user_input_float(
-                f"Enter maximum energy difference (kJ mol-1) between sum of IQA and WFN, default {default_float_difference_iqa_wfn}: "  # noqa: E501
-            )
-            if float_difference_iqa_wfn is None:
-                float_difference_iqa_wfn = default_float_difference_iqa_wfn
-        else:
-            float_difference_iqa_wfn = math.inf
-
-        # filtering by integration error
-        filter_by_integration_error = user_input_bool(
-            f"Filter by integration error (yes/no), default {bool_to_str(default_filter_by_integration_error)}: "
-        )
-        if filter_by_integration_error is None:
-            filter_by_integration_error = default_filter_by_integration_error
-
-        # ask for integration error if user has said yes
-        if filter_by_integration_error:
-            float_integration_error = user_input_float(
-                f"Enter integration error threshold, default {default_float_integration_error}: "
-            )
-            if float_integration_error is None:
-                float_integration_error = default_float_integration_error
-        # if user has selected do not filter by integration error, then set threshold to infinity
-        else:
-            float_integration_error = math.inf
-
-        submit_on_compute = user_input_bool(
-            f"Submit to compute node (yes/no), default {bool_to_str(default_submit_on_compute)}: "
-        )
-        if submit_on_compute is None:
-            submit_on_compute = default_submit_on_compute
-
-        ncores = user_input_int(
-            f"Number of cores to use (one core per atom csv), default {default_ncores}: "
-        )
-        if ncores is None:
-            ncores = default_ncores
-
-        if not submit_on_compute:
-            alf = get_alf_from_first_db_geometry(db_path)
-            write_processed_data_for_atoms_parallel(
-                db_path,
-                alf,
-                ncores,
-                max_diff_iqa_wfn=float_difference_iqa_wfn,
-                max_integration_error=float_integration_error,
-                calc_multipoles=rotate_multipoles,
-                calc_forces=calculate_feature_forces,
-            )
-
-        # if running on compute
-        else:
-            text_list = []
-            # make the python command that will be written in the submit script
-            # it will get executed as `python -c python_code_to_execute...`
-            text_list.append(
-                "from ichor.core.sql.query_database import write_processed_data_for_atoms_parallel"
-            )
-            text_list.append(
-                "from ichor.core.sql.query_database import get_alf_from_first_db_geometry"
-            )
-            text_list.append("from pathlib import Path")
-            text_list.append(f"db_path = Path('{db_path.absolute()}')")
-            text_list.append("alf = get_alf_from_first_db_geometry(db_path)")
-            str_part1 = (
-                f"write_processed_data_for_atoms_parallel(db_path, alf, {ncores},"
-            )
-            str_part2 = f" calc_multipoles={rotate_multipoles}, calc_forces={calculate_feature_forces})"
-            text_list.append(str_part1 + str_part2)
-
-            final_cmd = compile_strings_to_python_code(text_list)
-            py_cmd = FreeFlowPythonCommand(final_cmd)
-            with SubmissionScript(
-                SCRIPT_NAMES["calculate_features"], ncores=ncores
-            ) as submission_script:
-                submission_script.add_command(py_cmd)
-            submission_script.submit()
 
 
 # initialize menu
@@ -217,10 +88,6 @@ point_directory_menu_items = [
         SUBMIT_DATABASE_MENU_DESCRIPTION.title,
         submit_database_menu,
         points_directory_menu,
-    ),
-    FunctionItem(
-        "Make processed csvs from SQLite3 database",
-        PointsDirectoryFunctions.make_csvs_from_database,
     ),
 ]
 
