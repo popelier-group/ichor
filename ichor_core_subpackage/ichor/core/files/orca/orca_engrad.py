64c918a4b59bef0e7b53a41514de3203c2f8c5dd
m-julian <52214154+m-julian@users.noreply.github.com>
2024-01-06 21:04:09 +0000

    add orca engrad

---
+++ ichor_core_subpackage/ichor/core/files/orca/orca_engrad.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+
+import numpy as np
+from ichor.core.atoms import Atom, Atoms
+from ichor.core.common.constants import type2nuclear_charge
+from ichor.core.common.functools import classproperty
+
+from ichor.core.common.types.forces import AtomForce
+
+# from enum import Enum
+from ichor.core.files.file import FileContents, ReadFile
+from ichor.core.files.file_data import HasAtoms
+
+nuclear_charge2type = {int(v): k for k, v in type2nuclear_charge.items()}
+
+
+class OrcaEngrad(ReadFile, HasAtoms):
+    """
+    Reads file containing the gradient calculated by ORCA.
+
+    References:
+        https://sites.google.com/site/orcainputlibrary/home
+        https://www.cup.uni-muenchen.de/oc/zipse/teaching/computational-chemistry-2/topics/a-typical-orca-output-file/
+        https://www.orcasoftware.de/tutorials_orca/first_steps/input_output.html
+        https://www.afs.enea.it/software/orca/orca_manual_4_2_1.pdf (note this is for version 4, not 5)
+        version 5 manual, needs login:
+        available in https://orcaforum.kofo.mpg.de/app.php/dlext/?view=detail&df_id=186
+        https://orcaforum.kofo.mpg.de/viewtopic.php?f=8&t=7470&p=32102&hilit=atomic+force#p32102
+    """
+
+    def __init__(self, path: Path | str):
+
+        super(ReadFile, self).__init__(path)
+        self.global_forces = FileContents
+        self.total_energy = FileContents
+        self.atoms = FileContents
+
+    @classproperty
+    def filetype(self) -> str:
+        return ".engrad"
+
+    @property
+    def gradient(self) -> np.ndarray:
+
+        return -self.global_forces
+
+    def _read_file(self):
+
+        atoms = Atoms()
+        forces = {}
+
+        with open(self.path, "r") as f:
+            # assume the first lines start with !
+            # the next lines are optional commands with %
+            # and finally the inputs
+
+            for line in f:
+
+                if "Number of atoms" in line:
+
+                    line = next(f)
+                    line = next(f)
+                    natoms = int(line.strip())
+
+                elif "The current total energy" in line:
+
+                    line = next(f)
+                    line = next(f)
+                    total_energy = float(line.strip())
+
+                elif "The current gradient" in line:
+
+                    line = next(f)
+
+                    gradient_array = []
+
+                    # every atom has x,y,z forces so multiply by 3
+                    for _ in range(3 * natoms):
+                        line = next(f).strip()
+                        gradient_array.append(float(line))
+
+                    gradient_array = np.array(gradient_array).reshape(-1, 3)
+
+                elif "The atomic numbers and current coordinates" in line:
+
+                    line = next(f)
+                    line = next(f)
+
+                    while line.strip():
+                        line_split = line.split()
+                        line_split[0] = nuclear_charge2type[int(line_split[0])]
+                        atoms.append(Atom(*line_split))
+                        try:
+                            line = next(f)
+                        except StopIteration:
+                            break
+
+            for atom_name, atom_gradient in zip(atoms.atom_names, gradient_array):
+                # the force is -ve of gradient
+                forces[atom_name] = AtomForce(*-atom_gradient)
+
+            self.global_forces = forces
+            self.atoms = atoms
+            self.total_energy = total_energy
