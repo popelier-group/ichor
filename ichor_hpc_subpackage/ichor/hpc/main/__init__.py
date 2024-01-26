0b64430b6e341846a77394b85874bdf4b84d0e59
m-julian <52214154+m-julian@users.noreply.github.com>
2024-01-26 17:33:09 +0000

    add to init

--- ichor_hpc_subpackage/ichor/hpc/main/__init__.py
+++ ichor_hpc_subpackage/ichor/hpc/main/__init__.py
@@ -1,10 +1,12 @@
 from ichor.hpc.main.aimall import submit_points_directory_to_aimall
 from ichor.hpc.main.database import submit_make_csvs_from_database
 from ichor.hpc.main.gaussian import submit_gjfs, submit_points_directory_to_gaussian
+from ichor.hpc.main.orca import submit_points_directory_to_orca
 
 __all__ = [
     "submit_points_directory_to_aimall",
     "submit_points_directory_to_gaussian",
     "submit_gjfs",
     "submit_make_csvs_from_database",
+    "submit_points_directory_to_orca",
 ]
