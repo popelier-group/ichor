faf768a75e0755ad6b2198405c6297b81d791161
m-julian <52214154+m-julian@users.noreply.github.com>
2024-01-28 23:02:41 +0000

    rename columns

--- ichor_core_subpackage/ichor/core/database/sql/query_database.py
+++ ichor_core_subpackage/ichor/core/database/sql/query_database.py
@@ -160,10 +160,14 @@ def raw_one_atom_data_to_df(
 
     df = pd.read_sql(stmt, conn)
 
+    # need to rename because the atom table has column "name"
+    # and points table also has column "name"
+    df = df.rename(columns={"name_1": "atom_name"})
+
     # drop columns which contain IDs and other things from SQLAlchemy query
     if drop_irrelevant_cols:
         df = df.drop(
-            ["id", "id_1", "point_id", "atom_id", "name_1", "id_2"], axis="columns"
+            ["id", "id_1", "point_id", "atom_id", "atom_name", "id_2"], axis="columns"
         )
 
     return df
@@ -224,12 +228,14 @@ def get_list_of_atom_names_from_db(db_path: Union[str, Path], echo=False) -> Lis
 
 
 def get_full_dataframe_for_all_atoms(
-    db_path: Union[str, Path], echo=False
+    db_path: Union[str, Path], echo=False, change_cols=True
 ) -> pd.DataFrame:
     """Returns a dataframe containing all the data for all atoms
 
     :param db_path: Path or str to database
     :param echo: Whether to echo SQL statements, defaults to False
+    :param change_cols: Removes some unnecessary columns and also renames some columns
+        to be more clear.
     :return: A pandas dataframe containing information for all atoms
     """
 
@@ -245,6 +251,14 @@ def get_full_dataframe_for_all_atoms(
     # )
 
     full_df = pd.read_sql(stmt, conn)
+
+    # need to rename because the atom table has column "name"
+    # and points table also has column "name"
+    full_df = full_df.rename(columns={"name_1": "atom_name"})
+
+    # drop columns which contain IDs and other things from SQLAlchemy query
+    if change_cols:
+        full_df = full_df.drop(["id_1", "point_id", "atom_id", "id_2"], axis="columns")
 
     return full_df
 
@@ -265,7 +279,7 @@ def get_alf_from_first_db_geometry(db_path: Union[str, Path], echo=False) -> Lis
     one_point_df = full_df.loc[full_df["id"] == first_point_id]
 
     for row_id, row_data in one_point_df.iterrows():
-        atom_type = get_characters(row_data.name_1)
+        atom_type = get_characters(row_data.atom_name)
         atoms.append(Atom(atom_type, row_data.x, row_data.y, row_data.z))
 
     return atoms.alf_list(calculate_alf_atom_sequence)
@@ -331,7 +345,7 @@ def write_processed_one_atom_data_to_csv(
         # check that integration error is below threshold, otherwise do not calculate features
         # for the atom and do not add this point to training set for this atom.
         # if other atoms have good integration errors, the same point can be used in their training sets.
-        row_with_atom_info = one_point_df.loc[one_point_df["name_1"] == atom_name]
+        row_with_atom_info = one_point_df.loc[one_point_df["atom_name"] == atom_name]
 
         if calc_forces:
             if not one_point_df["force_x"].isnull().values.any():
@@ -357,7 +371,7 @@ def write_processed_one_atom_data_to_csv(
                 atoms = Atoms()
                 for row_id, row_data in one_point_df.iterrows():
                     # atoms accepts atom type (but database contains the atom index as well)
-                    atom_type = get_characters(row_data.name_1)
+                    atom_type = get_characters(row_data.atom_name)
                     atoms.append(Atom(atom_type, row_data.x, row_data.y, row_data.z))
 
                 natoms = len(atoms)
@@ -468,7 +482,7 @@ def write_processed_one_atom_data_to_csv(
             atoms = Atoms()
             for row_id, row_data in one_point_df.iterrows():
                 # atoms accepts atom type (but database contains the atom index as well)
-                atom_type = get_characters(row_data.name_1)
+                atom_type = get_characters(row_data.aton_name)
                 atoms.append(Atom(atom_type, row_data.x, row_data.y, row_data.z))
 
             C = atoms[atom_name].C(alf)
@@ -708,7 +722,7 @@ def atoms_from_point_id(full_df, point_id: int) -> "Atoms":
     atoms = Atoms()
     for row_id, row_data in one_point_df.iterrows():
         # atoms accepts atom type (but database contains the atom index as well)
-        atom_type = get_characters(row_data.name_1)
+        atom_type = get_characters(row_data.atom_name)
         atoms.append(Atom(atom_type, row_data.x, row_data.y, row_data.z))
 
     return atoms
@@ -761,7 +775,7 @@ def csv_file_with_specific_properties(
                 # for the atom and do not add this point to training set for this atom.
                 # if other atoms have good integration errors, the same point can be used in their training sets.
                 row_with_atom_info = one_point_df.loc[
-                    one_point_df["name_1"] == atom_name
+                    one_point_df["atom_name"] == atom_name
                 ]
                 results = [str(row_with_atom_info[p]) for p in properties]
                 f.write(",".join(results) + "\n")
