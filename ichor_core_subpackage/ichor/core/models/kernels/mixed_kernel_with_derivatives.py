0d1accbc63bb66a070958315e0f8a75b65256798
m-julian <52214154+m-julian@users.noreply.github.com>
2024-03-22 18:24:16 +0000

    numpy version

--- ichor_core_subpackage/ichor/core/models/kernels/mixed_kernel_with_derivatives.py
+++ ichor_core_subpackage/ichor/core/models/kernels/mixed_kernel_with_derivatives.py
@@ -11,6 +11,9 @@ class MixedKernelWithDerivatives(Kernel):
 
     Also adds first and second derivatives of the mixed kernel which are used to train on the total
     system energy with gradient information.
+
+    The data is assumed to be uscaled, i.e. the original data. The period length array is set for all
+    the dimensions even though it is only used for the periodic dimensions to vectorize the operations.
     """
 
     def __init__(
@@ -18,31 +21,39 @@ class MixedKernelWithDerivatives(Kernel):
         name: str,
         rbf_thetas: np.ndarray,
         periodic_thetas: np.ndarray,
-        periodic_period_lengths: np.ndarray,
         rbf_dimensions: np.ndarray,
         periodic_dimensions: np.ndarray,
     ):
 
-        ndims = len(rbf_dimensions + periodic_dimensions)
+        ndims = len(rbf_dimensions) + len(periodic_dimensions)
         active_dims = np.arange(ndims)
 
         super().__init__(name, active_dims)
         self._rbf_thetas = rbf_thetas
         self._periodic_thetas = periodic_thetas
-        self.period_length = periodic_period_lengths
+        # needs to be 1 x ndim to be able to fit with torch implementation
+        self.period_length = (np.ones(ndims) * (2 * np.pi))[np.newaxis, ...]
 
         self.rbf_dimensions = rbf_dimensions
         self.periodic_dimensions = periodic_dimensions
 
     @property
+    def params(self) -> np.ndarray:
+        pass
+
+    @property
     def rbf_lengthscales(self):
-        return np.sqrt(1.0 / (2 * self._rbf_thetas))
+        return 1.0 / (2.0 * self._rbf_thetas)
 
     @property
     def periodic_lengthscales(self):
         """Note that the lengthscales are already squared for the periodic kernel. But still,
         thetas are defined to be 1/(2l). (where l here is the already squared true lengthscale)"""
         return 1.0 / (2.0 * self._periodic_thetas)
+
+    @property
+    def ndims(self):
+        return len(self.active_dims)
 
     @property
     def lengthscale(self) -> np.ndarray:
@@ -53,13 +64,18 @@ class MixedKernelWithDerivatives(Kernel):
         iter_rbf_dims = iter(self.rbf_lengthscales)
         iter_periodic_lengthscales = iter(self.periodic_lengthscales)
 
-        for i in range(self.active_dims):
+        for i in range(len(self.active_dims)):
             if (i + 1) % 3 == 0 and i != 2:
                 l.append(next(iter_periodic_lengthscales))
             else:
                 l.append(next(iter_rbf_dims))
 
-        return np.array(l)
+        # needs to be 1 x ndim to be able to fit with torch implementation
+        return np.array(l)[np.newaxis, ...]
+
+    @property
+    def lengthscales(self):
+        return self.lengthscale
 
     def k(self, x1: np.ndarray, x2: np.ndarray) -> np.ndarray:
         """
@@ -84,28 +100,29 @@ class MixedKernelWithDerivatives(Kernel):
         n2 = x2.shape[-2]
 
         # this is the full matrix
-        K = np.zeros(*batch_shape, n1 * (d + 1), n2 * (d + 1), dtype=x1.dtype)
+        K = np.zeros((*batch_shape, n1 * (d + 1), n2 * (d + 1)), dtype=x1.dtype)
 
         # shape of n1 x n2 x d
-        diffs = x1.view(*batch_shape, n1, 1, d) - x2.view(*batch_shape, 1, n2, d)
+        diffs = x1.reshape(*batch_shape, n1, 1, d) - x2.reshape(*batch_shape, 1, n2, d)
 
         # rbf part of matrix
         # shape is n1 x n2 x nrbfdims
         rbf_part = diffs[..., self.rbf_dimensions] ** 2
-        rbf_part = rbf_part / lengthscale[..., self.rbf_dimensions].unsqueeze(-2)
+        rbf_part = rbf_part / np.expand_dims(lengthscale[..., self.rbf_dimensions], -2)
         # sum over nrbfdims
-        rbf_part = rbf_part.sum(-1).mul(-0.5)
+        rbf_part = rbf_part.sum(-1) * (-0.5)
 
         # # shape is n1 x n2 x nperiodic
-        periodic_part = diffs[..., self.periodic_dimensions].mul(
-            math.pi / self.period_length[..., self.periodic_dimensions].unsqueeze(-2)
+        periodic_part = diffs[..., self.periodic_dimensions] * (
+            np.expand_dims(
+                math.pi / self.period_length[..., self.periodic_dimensions], -2
+            )
         )
-        periodic_part = periodic_part.sin().pow(2.0)
-        periodic_part = (
-            periodic_part.div(lengthscale[..., self.periodic_dimensions].unsqueeze(-2))
-            .sum(-1)
-            .mul(-2.0)
-        )
+        periodic_part = np.sin(periodic_part) ** (2.0)
+        periodic_part = np.divide(
+            periodic_part,
+            np.expand_dims(lengthscale[..., self.periodic_dimensions], -2),
+        ).sum(-1) * (-2.0)
 
         # # 1) Kernel block
         # # directly use the already implemented RBF and PeriodicKernel methods
@@ -124,108 +141,102 @@ class MixedKernelWithDerivatives(Kernel):
         # possibly transform this kron back into a n1 x n2 x d matrix
         periodic_mask_dx_i_n = np.kron(
             periodic_dims_zeros_ones_tensor,
-            np.ones(*batch_shape, n1, n2, dtype=x1.dtype),
+            np.ones((*batch_shape, n1, n2), dtype=x1.dtype),
         )
         rbf_mask_dx_i_n = np.kron(
-            rbf_dims_zeros_ones_tensor, np.ones(*batch_shape, n1, n2, dtype=x1.dtype)
+            rbf_dims_zeros_ones_tensor, np.ones((*batch_shape, n1, n2), dtype=x1.dtype)
         )
 
         # outer1 here is the rbf part, but name as the final variable, and use inplace operations to save memory
         # divide each dimension by the lengthscale
-        outer1 = diffs / self.lengthscale.unsqueeze(-2)
+        outer1 = diffs / np.expand_dims(self.lengthscale, -2)
         # get n1 x n2 x d tensor
-        outer1 = np.transpose(outer1, -1, -2).contiguous()
+        outer1 = np.swapaxes(outer1, -1, -2)
+
         # get n1 x n2 * d tensor
         outer1 = outer1.reshape(*batch_shape, n1, n2 * d)
         # multiply by mask so that periodic dimensions are 0
-        outer1 = outer1.mul_(rbf_mask_dx_i_n.to_dense())
+        outer1 = outer1 * rbf_mask_dx_i_n
 
         outer_topright_periodic = diffs * (
-            ((2 * math.pi) / self.period_length.unsqueeze(-2))
+            ((2 * math.pi) / np.expand_dims(self.period_length, -2))
         )
         outer_topright_periodic = np.sin(outer_topright_periodic) * (
             2
             * math.pi
-            / (self.lengthscale.unsqueeze(-2) * self.period_length.unsqueeze(-2))
+            / (
+                np.expand_dims(self.lengthscale, -2)
+                * np.expand_dims(self.period_length, -2)
+            )
         )
-        outer_topright_periodic = np.transpose(
-            outer_topright_periodic, -1, -2
-        ).contiguous()
-        outer_topright_periodic = outer_topright_periodic.view(*batch_shape, n1, n2 * d)
-        outer_topright_periodic = (
-            outer_topright_periodic * periodic_mask_dx_i_n.to_dense()
+        outer_topright_periodic = np.swapaxes(outer_topright_periodic, -1, -2)
+        outer_topright_periodic = outer_topright_periodic.reshape(
+            *batch_shape, n1, n2 * d
         )
+        outer_topright_periodic = outer_topright_periodic * periodic_mask_dx_i_n
 
-        outer1.add_(outer_topright_periodic)
+        outer1 += outer_topright_periodic
 
-        K[..., :n1, n2:] = outer1 * K[..., :n1, :n2].repeat(
-            [*([1] * (n_batch_dims + 1)), d]
-        )
+        K[..., :n1, n2:] = outer1 * np.tile(K[..., :n1, :n2], d)
 
         # todo: implemt this as n1 x n2 x d directly and to work on the distance matrix, that will save some space
         periodic_mask_dx_j_m = np.kron(
             periodic_dims_zeros_ones_tensor,
-            np.ones(*batch_shape, n2, n1, dtype=x1.dtype),
-        ).transpose(-2, -1)
+            np.ones((*batch_shape, n2, n1), dtype=x1.dtype),
+        ).swapaxes(-2, -1)
         rbf_mask_dx_j_m = np.kron(
-            rbf_dims_zeros_ones_tensor, np.ones(*batch_shape, n2, n1, dtype=x1.dtype)
-        ).transpose(-2, -1)
+            rbf_dims_zeros_ones_tensor, np.ones((*batch_shape, n2, n1), dtype=x1.dtype)
+        ).swapaxes(-2, -1)
 
         # save some memory by naming outer_bottomleft_rbf as outer2
         # and use inplace operations
-        outer2 = diffs / self.lengthscale.unsqueeze(-2)  # n1 x n2 x d
-        outer2 = np.transpose(outer2, -1, -2).contiguous()  # n1 x d x n2
-        outer2 = outer2.transpose(-3, -1)  # n2 x d x n1
+        outer2 = diffs / np.expand_dims(self.lengthscale, -2)  # n1 x n2 x d
+        outer2 = np.swapaxes(outer2, -1, -2)  # n1 x d x n2
+        outer2 = outer2.swapaxes(-3, -1)  # n2 x d x n1
         outer2 = outer2.reshape(*batch_shape, n2, n1 * d)  # n2 x n1 * d
-        outer2 = outer2.transpose(-1, -2)  # n1 * d x n2
-        outer2.mul_(rbf_mask_dx_j_m.to_dense())  # n1 * d x n2
+        outer2 = outer2.swapaxes(-1, -2)  # n1 * d x n2
+        outer2 *= rbf_mask_dx_j_m  # n1 * d x n2
 
         outer_bottomleft_periodic = diffs * (
-            ((2 * math.pi) / self.period_length.unsqueeze(-2))
+            ((2 * math.pi) / np.expand_dims(self.period_length, -2))
         )  # n1 x n2 x d
         outer_bottomleft_periodic = np.sin(outer_bottomleft_periodic) * (
             2
             * math.pi
-            / (self.lengthscale.unsqueeze(-2) * self.period_length.unsqueeze(-2))
+            / (
+                np.expand_dims(self.lengthscale, -2)
+                * np.expand_dims(self.period_length, -2)
+            )
         )
-        outer_bottomleft_periodic = np.transpose(
+        outer_bottomleft_periodic = np.swapaxes(
             outer_bottomleft_periodic, -1, -2
-        ).contiguous()  # n1 x d x n2
-        outer_bottomleft_periodic = outer_bottomleft_periodic.transpose(
+        )  # n1 x d x n2
+        outer_bottomleft_periodic = outer_bottomleft_periodic.swapaxes(
             -3, -1
         )  # n2 x d x n1
         outer_bottomleft_periodic = outer_bottomleft_periodic.reshape(
             *batch_shape, n2, n1 * d
         )
-        outer_bottomleft_periodic = outer_bottomleft_periodic.transpose(-2, -1)
-        outer_bottomleft_periodic = (
-            outer_bottomleft_periodic * periodic_mask_dx_j_m.to_dense()
-        )
+        outer_bottomleft_periodic = outer_bottomleft_periodic.swapaxes(-2, -1)
+        outer_bottomleft_periodic = outer_bottomleft_periodic * periodic_mask_dx_j_m
 
-        outer2.add_(outer_bottomleft_periodic)
+        outer2 += outer_bottomleft_periodic
 
-        K[..., n1:, :n2] = -outer2 * K[..., :n1, :n2].repeat(
-            [*([1] * n_batch_dims), d, 1]
-        )
+        K[..., n1:, :n2] = -outer2 * np.tile(K[..., :n1, :n2], (d, 1))
 
         # make mask arrays for dxjm_dxin part of matrix.
-        rbf_mask_dxjm_dxin = rbf_mask_dx_i_n.to_dense().repeat(
-            [*([1] * n_batch_dims), d, 1]
-        ) * rbf_mask_dx_j_m.to_dense().repeat([*([1] * (n_batch_dims + 1)), d])
-        periodic_mask_dxjm_dxin = periodic_mask_dx_i_n.to_dense().repeat(
-            [*([1] * n_batch_dims), d, 1]
-        ) * periodic_mask_dx_j_m.to_dense().repeat([*([1] * (n_batch_dims + 1)), d])
+        rbf_mask_dxjm_dxin = np.tile(rbf_mask_dx_i_n.repeat, (d, 1)) * np.tile(
+            rbf_mask_dx_j_m, (1, d)
+        )
+        periodic_mask_dxjm_dxin = np.tile(periodic_mask_dx_i_n, (d, 1)) * np.tile(
+            periodic_mask_dx_j_m, (1, d)
+        )
 
         # make mask for elements where both rbf and periodic derivatives are needed
-        mask_i_P_j_R = rbf_mask_dx_i_n.to_dense().repeat(
-            [*([1] * n_batch_dims), d, 1]
-        ) * periodic_mask_dx_j_m.to_dense().repeat(
-            [*([1] * (n_batch_dims + 1)), d]
-        ) + periodic_mask_dx_i_n.to_dense().repeat(
-            [*([1] * n_batch_dims), d, 1]
-        ) * rbf_mask_dx_j_m.to_dense().repeat(
-            [*([1] * (n_batch_dims + 1)), d]
+        mask_i_P_j_R = np.tile(rbf_mask_dx_i_n, (d, 1)) * np.tile(
+            periodic_mask_dx_j_m, (1, d)
         )
+        +np.tile(periodic_mask_dx_i_n, (d, 1)) * np.tile(rbf_mask_dx_j_m, (1, d))
 
         outer3 = outer1.repeat([*([1] * n_batch_dims), d, 1]) * -outer2.repeat(
             [*([1] * (n_batch_dims + 1)), d]
@@ -245,14 +256,15 @@ class MixedKernelWithDerivatives(Kernel):
         )
 
         periodic_kp_outer3 = diffs * (
-            (2 * math.pi) / (self.period_length.unsqueeze(-2))
+            (2 * math.pi) / np.expand_dims(self.period_length, -2)
         )
         periodic_kp_outer3 = (
             (4 * math.pi**2)
-            / (self.period_length.unsqueeze(-2) * self.lengthscale.unsqueeze(-2))
+            / np.expand_dims(self.period_length, -2)
+            * np.expand_dims(self.lengthscale, -2)
         ) * np.cos(periodic_kp_outer3)
         periodic_kp_outer3 = np.transpose(periodic_kp_outer3, -1, -2).contiguous()
-        periodic_kp_outer3 = kp_periodic.to_dense() * periodic_kp_outer3.view(
+        periodic_kp_outer3 = kp_periodic.to_dense() * periodic_kp_outer3.reshape(
             *batch_shape, n1, n2 * d
         ).repeat([*([1] * n_batch_dims), d, 1])
 
