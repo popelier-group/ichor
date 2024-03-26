ba02b4814fa2969221192363cc8529bec3c26545
m-julian <52214154+m-julian@users.noreply.github.com>
2024-03-26 20:26:58 +0000

    add to init file

--- ichor_core_subpackage/ichor/core/models/__init__.py
+++ ichor_core_subpackage/ichor/core/models/__init__.py
@@ -1,7 +1,5 @@
 from ichor.core.models.model import Model
+from ichor.core.models.model_with_gradient import ModelWithGradients
 from ichor.core.models.models import Models
 
-__all__ = [
-    "Model",
-    "Models",
-]
+__all__ = ["Model", "Models", "ModelWithGradients"]
