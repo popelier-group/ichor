21c22220cd852942e762a8d4da7133aeb2e49c72
m-julian <52214154+m-julian@users.noreply.github.com>
2024-04-10 14:05:18 +0100

    increase default verbosity for gaussian output

--- ichor_core_subpackage/ichor/core/files/gaussian/gjf.py
+++ ichor_core_subpackage/ichor/core/files/gaussian/gjf.py
@@ -273,6 +273,7 @@ class GJF(ReadFile, WriteFile, HasAtoms):
             "output=wfn",
             "force",
             "geom=notest",
+            "IOp(1/33=2)",  # verbosity level 2, prints out B matrix, G matrix and G inverse to output file
         ]
 
         self.title = self.title or str(self.path.stem)
