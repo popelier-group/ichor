d0c9a936f4b0026c340e131b3efb2d7b11bd3611
m-julian <52214154+m-julian@users.noreply.github.com>
2024-05-12 20:48:46 +0100

    update to v4.0.3

--- ichor_core/ichor/core/__init__.py
+++ ichor_core/ichor/core/__init__.py
@@ -1,3 +1,7 @@
 from ichor.core.common.types import Version
 
-__version__ = Version("3.1.0")
+# TODO: removing this causes cyclic imports
+# fix these cyclic imports in future versions
+# since this is here, types is imported first which is why no
+# cyclic imports are seen...
+__version__ = Version("4.0.3")
