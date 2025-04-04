11d374fbb17ad8c9c1b8590b455aff708eb3161c
Olivia Aten <olivia.aten@student.manchester.ac.uk>
2025-04-04 13:00:27 +0000

    Initial structure commit

--- ichor_cli/ichor/cli/main_menu_submenus/initial_structure_menu/initial_structure_submenus/optimisation_submenus/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/initial_structure_menu/initial_structure_submenus/optimisation_submenus/__init__.py
@@ -1,9 +1,9 @@
-from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_submenus.optimisation_submenus.submit_gaussian_menu import (
+from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_submenus.optimisation_submenus.submit_gaussian_submenu import (
     submit_gaussian_menu,
     SUBMIT_GAUSSIAN_MENU_DESCRIPTION,
 )
 
-from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_submenus.optimisation_submenus.submit_plumed_menu import (
+from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_submenus.optimisation_submenus.submit_plumed_submenu import (
     submit_plumed_menu,
     SUBMIT_PLUMED_MENU_DESCRIPTION,
 )
