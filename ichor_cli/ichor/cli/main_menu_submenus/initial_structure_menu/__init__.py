2fe51ad2c8befea6902f00dd3d72ab37fb4d9724
Olivia Aten <olivia.aten@student.manchester.ac.uk>
2025-05-12 14:34:45 +0000

    initial structure test

--- ichor_cli/ichor/cli/main_menu_submenus/initial_structure_menu/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/initial_structure_menu/__init__.py
@@ -1,6 +1,4 @@
 from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_menu import (
-    AVAILABLE_READ_FILE_FORMATS,
-    AVAILABLE_WRITE_FILE_FORMATS,
     initial_structure_menu,
     INITIAL_STRUCTURE_MENU_DESCRIPTION,
 )
@@ -8,6 +6,4 @@ from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_menu 
 __all__ = [
     "initial_structure_menu",
     "INITIAL_STRUCTURE_MENU_DESCRIPTION",
-    "AVAILABLE_WRITE_FILE_FORMATS",
-    "AVAILABLE_READ_FILE_FORMATS",
 ]
