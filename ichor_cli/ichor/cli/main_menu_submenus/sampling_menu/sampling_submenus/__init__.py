0cf94626dff62e24de11123552380672e3404a90
Olivia Aten <olivia.aten@student.manchester.ac.uk>
2025-09-15 18:02:58 +0100

    fix __init__ for sampling menu

--- ichor_cli/ichor/cli/main_menu_submenus/sampling_menu/sampling_submenus/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/sampling_menu/sampling_submenus/__init__.py
@@ -1,9 +1,9 @@
-from ichor.cli.main_menu_submenus.initial_structure_menu.initial_structure_submenus.optimisation_submenus.submit_ase_submenu import (
-    submit_ase_menu,
-    SUBMIT_ASE_MENU_DESCRIPTION,
+from ichor.cli.main_menu_submenus.sampling_menu.sampling_submenus.diversity_submenu import (
+    submit_diversity_menu,
+    SUBMIT_DIVERSITY_MENU_DESCRIPTION,
 )
 
 __all__ = [
-    "submit_ase_menu",
-    "SUBMIT_ASE_MENU_DESCRIPTION",
+    "submit_diversity_menu",
+    "SUBMIT_DIVERSITY_MENU_DESCRIPTION",
 ]
