ffeae8fb5c50dc7513843063b1b1f0fa7026b5b5
Olivia Aten <olivia.aten@student.manchester.ac.uk>
2025-10-02 16:42:15 +0100

    bug fix

--- ichor_cli/ichor/cli/main_menu_submenus/training_menu/training_submenus/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/training_menu/training_submenus/__init__.py
@@ -1,9 +1,9 @@
 from ichor.cli.main_menu_submenus.training_menu.training_submenus.data_preparation_menu import (
-    data_preparation_menu,
+    submit_data_prep_menu,
     SUBMIT_DATA_PREP_MENU_DESCRIPTION,
 )
 
 __all__ = [
-    "data_preparation_menu",
+    "submit_data_prep_menu",
     "SUBMIT_DATA_PREP_MENU_DESCRIPTION",
 ]
