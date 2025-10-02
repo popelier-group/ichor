57515a3f371b0043c12b68ffb8be6fb7682e0dff
adamrhyshill <adamrhyshill@gmail.com>
2025-10-02 17:37:38 +0100

    initialised list for passing cvs back to menu

--- ichor_cli/ichor/cli/main_menu_submenus/trajectory_creation_menu/trajectory_creation_submenus/col_var_submenus/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/trajectory_creation_menu/trajectory_creation_submenus/col_var_submenus/__init__.py
@@ -1,9 +1,11 @@
 from ichor.cli.main_menu_submenus.trajectory_creation_menu.trajectory_creation_submenus.col_var_submenus.col_var_submenu import (
     col_var_menu,
     COL_VAR_MENU_DESCRIPTION,
+    collective_variables_list,
 )
 
 __all__ = [
     "COL_VAR_MENU_DESCRIPTION",
     "col_var_menu",
+    "collective_variable_list",
 ]
