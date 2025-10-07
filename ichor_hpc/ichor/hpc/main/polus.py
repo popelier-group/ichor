bd6b3d298b7fcf7b19d926bdb7e9eaa04d94ff61
Olivia Aten <olivia.aten@student.manchester.ac.uk>
2025-10-07 17:22:51 +0100

    bug fix

--- ichor_hpc/ichor/hpc/main/polus.py
+++ ichor_hpc/ichor/hpc/main/polus.py
@@ -46,7 +46,7 @@ def write_dataset_prep(
     input_filename = "datasets" + DatasetPrepScript.get_filetype()
 
     dataset_input_script = DatasetPrepScript(
-        Path(outlier_input_dir=Path(outlier_input_dir)),
+        outlier_input_dir=Path(outlier_input_dir),
         **kwargs,
     )
     dataset_input_script.write()
