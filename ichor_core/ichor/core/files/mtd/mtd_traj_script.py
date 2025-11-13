c9c135468f7400f69f8e0c1a05a2ebc4c58fb8a3
adamrhyshill <adamrhyshill@gmail.com>
2025-11-13 14:26:37 +0000

    missed change to single hills file

--- ichor_core/ichor/core/files/mtd/mtd_traj_script.py
+++ ichor_core/ichor/core/files/mtd/mtd_traj_script.py
@@ -144,7 +144,7 @@ class MtdTrajScript(WriteFile, File):
             cv_line = self.build_cv_str(self.collective_variables[0], 1, "")
             metad_line = f'\t"METAD ARG=m1 HEIGHT={self.height} PACE={self.pace} " +\n'
             sigma_line = f'\t"SIGMA={self.sigma[0]} GRID_MIN={self.grid_min[0]} GRID_MAX={self.grid_max[0]}" +\n'
-            grid_bin_line = f'\t" GRID_BIN={self.grid_bin[0]} BIASFACTOR={self.bias_factor} FILE={self.hills_files[0]}"]\n'
+            grid_bin_line = f'\t" GRID_BIN={self.grid_bin[0]} BIASFACTOR={self.bias_factor} FILE={self.hills_file}"]\n'
             setup_str = header_line + cv_line + metad_line + sigma_line + grid_bin_line
             return setup_str
         else:
