305e2a575f57e65d7ce9a6ee93085036859ed300
adamrhyshill <adamrhyshill@gmail.com>
2025-11-18 13:46:59 +0000

    attempt to merge plumed calc and mtd script

--- ichor_core/ichor/core/files/mtd/mtd_traj_script.py
+++ ichor_core/ichor/core/files/mtd/mtd_traj_script.py
@@ -188,12 +188,175 @@ class MtdTrajScript(WriteFile, File):
         mtd_traj_script_template = Template(
             textwrap.dedent(
                 """
+            from os.path import exists
+            import numpy as np
             from ase import units
+            from ase.units import fs, kJ, mol, nm
+            from ase.calculators.calculator import Calculator, all_changes
             from ase.io import read,write,Trajectory
             from xtb.ase.calculator import XTB
             from ase.md.langevin import Langevin
-            from ichor.core.files.mtd.plumed_calculator import Plumed
-            from ase.parallel import world
+            from ase.parallel import broadcast, world
+
+            def restart_from_trajectory(prev_traj, *args, prev_steps=None, atoms=None, **kwargs):
+                atoms.calc = Plumed(*args, atoms=atoms, restart=True, **kwargs)
+    
+                with Trajectory(prev_traj) as traj:
+                    if prev_steps is None:
+                        atoms.calc.istep = len(traj) - 1
+                    else:
+                        atoms.calc.istep = prev_steps
+                    atoms.set_positions(traj[-1].get_positions())
+                    atoms.set_momenta(traj[-1].get_momenta())
+                return atoms.calc
+
+
+            class Plumed(Calculator):
+                implemented_properties = ["energy", "forces"]
+
+                def __init__(
+                    self,
+                    calc,
+                    input,
+                    timestep,
+                    atoms=None,
+                    kT=1.0,
+                    log="",
+                    restart=False,
+                    use_charge=False,
+                    update_charge=False,
+                ):
+                    from plumed import Plumed as pl
+
+                    if atoms is None:
+                        raise TypeError(
+                            "plumed calculator has to be defined with the \
+                                         object atoms inside."
+                        )
+
+                    self.istep = 0
+                    Calculator.__init__(self, atoms=atoms)
+
+                    self.input = input
+                    self.calc = calc
+                    self.use_charge = use_charge
+                    self.update_charge = update_charge
+
+                    if world.rank == 0:
+                        natoms = len(atoms.get_positions())
+                        self.plumed = pl()
+                        ps = 1000 * fs
+                        self.plumed.cmd("setMDEnergyUnits", mol / kJ)
+                        self.plumed.cmd("setMDLengthUnits", 1 / nm)
+                        self.plumed.cmd("setMDTimeUnits", 1 / ps)
+                        self.plumed.cmd("setMDChargeUnits", 1.0)
+                        self.plumed.cmd("setMDMassUnits", 1.0)
+
+                        self.plumed.cmd("setNatoms", natoms)
+                        self.plumed.cmd("setMDEngine", "ASE")
+                        self.plumed.cmd("setLogFile", log)
+                        self.plumed.cmd("setTimestep", float(timestep))
+                        self.plumed.cmd("setRestart", restart)
+                        self.plumed.cmd("setKbT", float(kT))
+                        self.plumed.cmd("init")
+                        for line in input:
+                            self.plumed.cmd("readInputLine", line)
+                    self.atoms = atoms
+
+                def _get_name(self):
+                    return f"{self.calc.name}+Plumed"
+
+                def calculate(
+                    self, atoms=None, properties=["energy", "forces"], system_changes=all_changes
+                ):
+                    Calculator.calculate(self, atoms, properties, system_changes)
+
+                    comp = self.compute_energy_and_forces(self.atoms.get_positions(), self.istep)
+                    energy, forces = comp
+                    self.istep += 1
+                    self.results["energy"], self.results["forces"] = energy, forces
+
+                def compute_energy_and_forces(self, pos, istep):
+                    unbiased_energy = self.calc.get_potential_energy(self.atoms)
+                    unbiased_forces = self.calc.get_forces(self.atoms)
+
+                    if world.rank == 0:
+                        ener_forc = self.compute_bias(pos, istep, unbiased_energy)
+                    else:
+                        ener_forc = None
+                    energy_bias, forces_bias = broadcast(ener_forc)
+                    energy = unbiased_energy + energy_bias
+                    forces = unbiased_forces + forces_bias
+                    return energy, forces
+
+                def compute_bias(self, pos, istep, unbiased_energy):
+                    self.plumed.cmd("setStep", istep)
+
+                    if self.use_charge:
+                        if "charges" in self.calc.implemented_properties and self.update_charge:
+                            charges = self.calc.get_charges(atoms=self.atoms.copy())
+
+                        elif self.atoms.has("initial_charges") and not self.update_charge:
+                            charges = self.atoms.get_initial_charges()
+
+                        else:
+                            assert not self.update_charge, "Charges cannot be updated"
+                            assert self.update_charge, "Not initial charges in Atoms"
+
+                        self.plumed.cmd("setCharges", charges)
+
+                    # Box for functions with PBC in plumed
+                    if self.atoms.cell:
+                        cell = np.asarray(self.atoms.get_cell())
+                        self.plumed.cmd("setBox", cell)
+
+                    self.plumed.cmd("setPositions", pos)
+                    self.plumed.cmd("setEnergy", unbiased_energy)
+                    self.plumed.cmd("setMasses", self.atoms.get_masses())
+                    forces_bias = np.zeros((self.atoms.get_positions()).shape)
+                    self.plumed.cmd("setForces", forces_bias)
+                    virial = np.zeros((3, 3))
+                    self.plumed.cmd("setVirial", virial)
+                    self.plumed.cmd("prepareCalc")
+                    self.plumed.cmd("performCalc")
+                    energy_bias = np.zeros((1,))
+                    self.plumed.cmd("getBias", energy_bias)
+                    return [energy_bias, forces_bias]
+
+                def write_plumed_files(self, images):
+                    for i, image in enumerate(images):
+                        pos = image.get_positions()
+                        self.compute_energy_and_forces(pos, i)
+                    return self.read_plumed_files()
+
+                def read_plumed_files(self, file_name=None):
+                    read_files = {}
+                    if file_name is not None:
+                        read_files[file_name] = np.loadtxt(file_name, unpack=True)
+                    else:
+                        for line in self.input:
+                            if line.find("FILE") != -1:
+                                ini = line.find("FILE")
+                                end = line.find(" ", ini)
+                                if end == -1:
+                                    file_name = line[ini + 5 :]
+                                else:
+                                    file_name = line[ini + 5 : end]
+                                read_files[file_name] = np.loadtxt(file_name, unpack=True)
+
+                        if len(read_files) == 0:
+                            if exists("COLVAR"):
+                                read_files["COLVAR"] = np.loadtxt("COLVAR", unpack=True)
+                            if exists("HILLS"):
+                                read_files["HILLS"] = np.loadtxt("HILLS", unpack=True)
+                    assert len(read_files) != 0, "There are not files for reading"
+                    return read_files
+
+                def __enter__(self):
+                    return self
+
+                def __exit__(self, *args):
+                    self.plumed.finalize()
 
             timestep = $time_step
             ps = $time_units
@@ -264,7 +427,7 @@ class MtdTrajScript(WriteFile, File):
             md_interval=self.md_interval,
             md_runsteps=self.md_runsteps,
             mtd_out_file=self.input_xyz_path.with_name(
-                self.system_name + "mtd_out.xyz"
+                self.system_name + "_mtd_out.xyz"
             ),
         )
 
