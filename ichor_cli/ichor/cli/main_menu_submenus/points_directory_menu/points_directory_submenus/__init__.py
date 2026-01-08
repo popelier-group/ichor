35ae1e50cb781b2934f0fd4e6865913dfd55d641
adamrhyshill <adamrhyshill@gmail.com>
2026-01-08 14:22:39 +0000

    moved traj splitting to submenu

--- ichor_cli/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_submenus/__init__.py
+++ ichor_cli/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_submenus/__init__.py
@@ -10,12 +10,18 @@ from ichor.cli.main_menu_submenus.points_directory_menu.points_directory_submenu
     submit_gaussian_menu,
     SUBMIT_GAUSSIAN_MENU_DESCRIPTION,
 )
+from ichor.cli.main_menu_submenus.points_directory_menu.points_directory_submenus.traj_split_menu import (
+    traj_split_menu,
+    TRAJ_SPLIT_MENU_DESCRIPTION,
+)
 
 __all__ = [
     "submit_aimall_menu",
     "submit_gaussian_menu",
     "submit_database_menu",
+    "traj_split_menu",
     "SUBMIT_AIMALL_MENU_DESCRIPTION",
     "SUBMIT_GAUSSIAN_MENU_DESCRIPTION",
     "SUBMIT_DATABASE_MENU_DESCRIPTION",
+    "TRAJ_SPLIT_MENU_DESCRIPTION",
 ]
