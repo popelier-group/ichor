3f2e42f2a41e0b47cbbbab9f6b624537e3c43537
adamrhyshill <adamrhyshill@gmail.com>
2026-01-08 14:11:02 +0000

    clear last commit - error pull

--- ichor_cli/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_menu.py
+++ ichor_cli/ichor/cli/main_menu_submenus/points_directory_menu/points_directory_menu.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Union
 
@@ -16,12 +16,12 @@ from ichor.cli.main_menu_submenus.points_directory_menu.points_directory_submenu
 )
 from ichor.cli.menu_description import MenuDescription
 from ichor.cli.menu_options import MenuOptions
-from ichor.cli.useful_functions import user_input_path
-from ichor.core.files import PointsDirectory, PointsDirectoryParent
+from ichor.cli.useful_functions import user_input_path, user_input_bool, user_input_int
+from ichor.core.files import PointsDirectory, PointsDirectoryParent, Trajectory
 
 POINTS_DIRECTORY_MENU_DESCRIPTION = MenuDescription(
     "PointsDirectory Menu",
-    subtitle="Use this to interact with ichor's PointsDirectory class.\n",
+    subtitle="Use this to create and interact with ichor's PointsDirectory class.\n",
 )
 
 
@@ -29,7 +29,19 @@ POINTS_DIRECTORY_MENU_DESCRIPTION = MenuDescription(
 @dataclass
 class PointsDirectoryMenuOptions(MenuOptions):
     # defaults to the current working directory
-    selected_points_directory_path: Path
+    selected_points_directory_path: Path = field(default_factory=lambda: Path.cwd())
+
+    selected_trajectory_path: Path = (
+        ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH
+    )
+
+    def check_selected_trajectory_path(self) -> Union[str, None]:
+        """Checks whether the given Trjectory exists or if it is a file."""
+        traj_path = Path(self.selected_trajectory_path)
+        if not traj_path.exists():
+            return f"Current trajectory path: {traj_path} does not exist."
+        elif not traj_path.is_file():
+            return f"Current trajectory path: {traj_path} is not a file."
 
     def check_path(self):
 
@@ -57,6 +69,71 @@ class PointsDirectoryFunctions:
     """Functions that run when menu items are selected"""
 
     @staticmethod
+    def select_trajectory():
+        """function that asks user to update trajectory path."""
+        traj_path = user_input_path("Enter Trajectory Path: ")
+        ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH = Path(
+            traj_path
+        ).absolute()
+        points_directory_menu_options.selected_trajectory_path = (
+            ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH
+        )
+
+    @staticmethod
+    def split_trajectory_into_points_directory():
+        """functions that splits a trajectory into a PointsDirectory."""
+        default_to_center = True
+        default_every = 1
+
+        to_center = user_input_bool(
+            f"Subtract centroid of geometries, default {default_to_center}: "
+        )
+        if to_center is None:
+            to_center = default_to_center
+
+        every = user_input_int(
+            f"Write out every ith geometry, default {default_every}: "
+        )
+        if every is None:
+            every = default_every
+
+        traj = Trajectory(ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH)
+        system_name = ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH.stem
+
+        traj.to_dir(system_name, every, to_center)
+
+    @staticmethod
+    def split_trajectory_into_many_points_directories():
+        """function that splits a given trajectory into many PointsDirectory-like directories."""
+        default_split_size = 5000
+        default_to_center = True
+        default_every = 1
+
+        nsplit = user_input_int(
+            f"Enter split size integer (default {default_split_size}): "
+        )
+        if nsplit is None:
+            nsplit = default_split_size
+
+        to_center = user_input_bool(
+            f"Subtract Centroid of Geometries (default {default_to_center}): "
+        )
+        if to_center is None:
+            to_center = default_to_center
+
+        every = user_input_int(
+            f"Write out every ith geometry (give i) (default {default_every}): "
+        )
+        if every is None:
+            every = default_every
+
+        traj = Trajectory(ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH)
+
+        system_name = ichor.cli.global_menu_variables.SELECTED_TRAJECTORY_PATH.stem
+
+        traj.to_dirs(system_name, nsplit, every, to_center)
+
+    @staticmethod
     def select_points_directory():
         """Asks user to update points directory and then updates PointsDirectoryMenuOptions instance."""
         pd_path = user_input_path("Change PointsDirectory Path: ")
@@ -81,6 +158,17 @@ points_directory_menu = ConsoleMenu(
 # make menu items
 # can use lambda functions to change text of options as well :)
 point_directory_menu_items = [
+    FunctionItem(
+        "Select path of trajectory", PointsDirectoryFunctions.select_trajectory
+    ),
+    FunctionItem(
+        "Split trajectory into single PointsDirectory-like dir",
+        PointsDirectoryFunctions.split_trajectory_into_points_directory,
+    ),
+    FunctionItem(
+        "Split trajectory into many PointsDirectories-like dirs",
+        PointsDirectoryFunctions.split_trajectory_into_many_points_directories,
+    ),
     FunctionItem(
         "Select PointsDirectory Path or Parent to PointsDirectory",
         PointsDirectoryFunctions.select_points_directory,
