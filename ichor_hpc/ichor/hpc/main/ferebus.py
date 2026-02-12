dade7cbafd23f0062ddd52c18a37b452235c22b1
Olivia Aten <30635071+oliviaaten@users.noreply.github.com>
2026-02-12 22:01:11 +0000

    add submit pyferebus

--- ichor_hpc/ichor/hpc/main/ferebus.py
+++ ichor_hpc/ichor/hpc/main/ferebus.py
@@ -46,43 +46,43 @@ def write_extract_models_script(
     return extract_models_script.path
 
 
-# def submit_pyferebus(
-#     input_script: Path,
-#     script_name: Optional[Union[str, Path]],
-#     hold: Optional[JobID] = None,
-#     ncores=2,
-#     outputs_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["outputs"],
-#     errors_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["errors"],
-#     **kwargs,
-# ) -> JobID:
-#     """Function that writes out a submission script which contains an array of
-#     Gaussian jobs to be ran on compute nodes. If calling this function from
-#     a log-in node, it will write out the submission script, a datafile (file which contains the names of
-#     all the .gjf file that need to be ran through Gaussian),
-#     and it will submit the submission script to compute nodes as well to run Gaussian on compute nodes.
-#     However, if using this function from a compute node,
-#     (which will happen when ichor is ran in auto-run mode), this function will only be used to write out
-#     the datafile and will not submit any new jobs
-#     from the compute node (as you cannot submit jobs from compute nodes on CSF3.)
+def submit_pyferebus(
+    input_script: Path,
+    script_name: Optional[Union[str, Path]],
+    hold: Optional[JobID] = None,
+    ncores=2,
+    outputs_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["outputs"],
+    errors_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["errors"],
+    **kwargs,
+) -> JobID:
+    """Function that writes out a submission script which contains an array of
+    Gaussian jobs to be ran on compute nodes. If calling this function from
+    a log-in node, it will write out the submission script, a datafile (file which contains the names of
+    all the .gjf file that need to be ran through Gaussian),
+    and it will submit the submission script to compute nodes as well to run Gaussian on compute nodes.
+    However, if using this function from a compute node,
+    (which will happen when ichor is ran in auto-run mode), this function will only be used to write out
+    the datafile and will not submit any new jobs
+    from the compute node (as you cannot submit jobs from compute nodes on CSF3.)
 
-#     :param gjfs: A list of Path objects pointing to .gjf files
-#     :param force_calculate_wfn: Run Gaussian calculations on given .gjf files,
-#         even if .wfn files already exist. Defaults to False.
-#     :script_name: Path to write submission script out to defaults to ichor.hpc.global_variables.SCRIPT_NAMES["gaussian"]
-#     :param hold: An optional JobID for which this job to hold.
-#         This is used in auto-run to hold this job for the previous job to finish, defaults to None
-#     :return: The JobID of this job given by the submission system.
-#     """
+    :param gjfs: A list of Path objects pointing to .gjf files
+    :param force_calculate_wfn: Run Gaussian calculations on given .gjf files,
+        even if .wfn files already exist. Defaults to False.
+    :script_name: Path to write submission script out to defaults to ichor.hpc.global_variables.SCRIPT_NAMES["gaussian"]
+    :param hold: An optional JobID for which this job to hold.
+        This is used in auto-run to hold this job for the previous job to finish, defaults to None
+    :return: The JobID of this job given by the submission system.
+    """
 
-#     # make a SubmissionScript instance which is going to contain all the jobs that are going to be ran
-#     # the submission_script object can be accessed even after the context manager
-#     with SubmissionScript(
-#         script_name,
-#         ncores=ncores,
-#         outputs_dir_path=outputs_dir_path,
-#         errors_dir_path=errors_dir_path,
-#     ) as submission_script:
+    # make a SubmissionScript instance which is going to contain all the jobs that are going to be ran
+    # the submission_script object can be accessed even after the context manager
+    with SubmissionScript(
+        script_name,
+        ncores=ncores,
+        outputs_dir_path=outputs_dir_path,
+        errors_dir_path=errors_dir_path,
+    ) as submission_script:
 
-#         submission_script.add_command(PythonCommand(input_script))
+        submission_script.add_command(PythonCommand(input_script))
 
-#     return submission_script.submit(hold=hold)
+    return submission_script.submit(hold=hold)
