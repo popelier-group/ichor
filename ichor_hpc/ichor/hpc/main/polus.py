36b39cbf45feab2e1c76c89ad8665810a5893e4d
Olivia Aten <30635071+oliviaaten@users.noreply.github.com>
2026-02-12 20:26:49 +0000

    test

--- ichor_hpc/ichor/hpc/main/polus.py
+++ ichor_hpc/ichor/hpc/main/polus.py
@@ -71,6 +71,7 @@ def submit_polus(
     ncores=2,
     outputs_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["outputs"],
     errors_dir_path=ichor.hpc.global_variables.FILE_STRUCTURE["errors"],
+    cwd=cwd,
     **kwargs,
 ) -> JobID:
     """Function that writes out a submission script which contains an array of
