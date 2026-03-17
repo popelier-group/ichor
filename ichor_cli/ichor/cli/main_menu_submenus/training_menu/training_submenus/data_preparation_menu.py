21065a7e0c72406ca43b732ee56ba463b42b5dd5
adamrhyshill <adamrhyshill@gmail.com>
2026-03-17 20:57:37 +0000

    removed path resolve

--- ichor_cli/ichor/cli/main_menu_submenus/training_menu/training_submenus/data_preparation_menu.py
+++ ichor_cli/ichor/cli/main_menu_submenus/training_menu/training_submenus/data_preparation_menu.py
@@ -204,7 +204,7 @@ class SubmitDataPrepFunctions:
         submit_polus(
             input_script=dataset_script,
             script_name=ichor.hpc.global_variables.SCRIPT_NAMES["datasets"],
-            cwd=Path(ichor.hpc.global_variables.FILE_STRUCTURE["datasets"]).resolve(),
+            cwd=Path(ichor.hpc.global_variables.FILE_STRUCTURE["datasets"]),
             ncores=ncores,
         )
 
