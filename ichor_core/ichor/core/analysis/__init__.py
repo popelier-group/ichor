ec19034549ea0fee4c2acc7eb055e4c111c69007
Olivia Aten <30635071+oliviaaten@users.noreply.github.com>
2026-08-05 19:58:52 +0100

    fix init for convex hulls

--- ichor_core/ichor/core/analysis/__init__.py
+++ ichor_core/ichor/core/analysis/__init__.py
@@ -1,9 +1,11 @@
 from ichor.core.analysis.dlpoly import DlpolyStabilityCheck, RunStability
 from ichor.core.analysis.trajectory_analysis import Stability, TrajectoryAnalysis
+from ichor.core.analysis.convex_hulls import ConvexHullAnalysis
 
 __all__ = [
     "TrajectoryAnalysis",
     "Stability",
     "DlpolyStabilityCheck",
     "RunStability",
+    "ConvexHullAnalysis",
 ]
