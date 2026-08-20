91debdabf07361fc7c7e0c7b879e4b48d887a2f6
adamrhyshill <adamrhyshill@gmail.com>
2026-08-20 17:41:40 +0100

    added menu for gaussian and aimall resubs - removed tools

--- ichor_cli/ichor/cli/main_menu_submenus/tools_menu/__init__.py
+++
@@ -1,6 +0,0 @@
-from ichor.cli.main_menu_submenus.tools_menu.tools_menu import (
-    tools_menu,
-    TOOLS_MENU_DESCRIPTION,
-)
-
-__all__ = ["tools_menu", "TOOLS_MENU_DESCRIPTION"]
--- ichor_cli/ichor/cli/main_menu_submenus/tools_menu/tools_submenus/__init__.py
+++
@@ -1,9 +0,0 @@
-from ichor.cli.main_menu_submenus.tools_menu.tools_submenus.points_directory_tools_submenu import (  # noqa: E501
-    points_directory_tools_menu,
-    POINTS_DIRECTORY_TOOLS_MENU_DESCRIPTION,
-)
-
-__all__ = [
-    "points_directory_tools_menu",
-    "POINTS_DIRECTORY_TOOLS_MENU_DESCRIPTION",
-]
